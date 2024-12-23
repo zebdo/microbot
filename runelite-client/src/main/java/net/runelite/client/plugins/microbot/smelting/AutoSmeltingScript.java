6aa3c06c85ac133699c8363a644efe1c781baae4
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2024-12-23 09:10:12 -0500

    fix: re-worked logic in autosmelting plugin to not rely on waitForInventoryChanges, this doesn't work with iron ore & ring of forging as its checking just inventory size change

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/smelting/AutoSmeltingScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/smelting/AutoSmeltingScript.java
@@ -7,6 +7,7 @@ import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
 import net.runelite.client.plugins.microbot.smelting.enums.Ores;
 import net.runelite.client.plugins.microbot.util.antiban.Rs2Antiban;
+import net.runelite.client.plugins.microbot.util.antiban.Rs2AntibanSettings;
 import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
 import net.runelite.client.plugins.microbot.util.equipment.Rs2Equipment;
 import net.runelite.client.plugins.microbot.util.gameobject.Rs2GameObject;
@@ -19,108 +20,107 @@ import java.text.MessageFormat;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-
 public class AutoSmeltingScript extends Script {
 
-    public static String version = "1.0.2";
-    private boolean expectingXPDrop = false;
+	public static String version = "1.0.3";
 
-    public boolean run(AutoSmeltingConfig config) {
+	public boolean run(AutoSmeltingConfig config) {
+		initialPlayerLocation = null;
+		Rs2Walker.disableTeleports = true;
+		Rs2Antiban.resetAntibanSettings();
+		Rs2Antiban.antibanSetupTemplates.applySmithingSetup();
+		mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
+			try {
+				if (!super.run()) return;
+				if (!Microbot.isLoggedIn()) return;
+				if (config.SELECTED_BAR_TYPE().getRequiredSmithingLevel() > Rs2Player.getBoostedSkillLevel(Skill.SMITHING)) {
+					Microbot.showMessage("Your smithing level isn't high enough for " + config.SELECTED_BAR_TYPE().toString());
+					super.shutdown();
+					return;
+				}
 
-        initialPlayerLocation = null;
-        mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
-            try {
-                if (!super.run()) return;
-                if (!Microbot.isLoggedIn()) return;
-                if (config.SELECTED_BAR_TYPE().getRequiredSmithingLevel() > Rs2Player.getBoostedSkillLevel(Skill.SMITHING)) {
-                    Microbot.showMessage("Your smithing level isn't high enough for " + config.SELECTED_BAR_TYPE().toString());
-                    super.shutdown();
-                    return;
-                }
+				if (initialPlayerLocation == null) {
+					initialPlayerLocation = Rs2Player.getWorldLocation();
+				}
 
-                if (initialPlayerLocation == null) {
-                    initialPlayerLocation = Rs2Player.getWorldLocation();
-                }
+				if (Rs2Player.isMoving() || Rs2Player.isAnimating(6500) || Microbot.pauseAllScripts) return;
+				if (Rs2AntibanSettings.actionCooldownActive) return;
 
-                if (Rs2Player.isMoving() || Rs2Player.isAnimating() || Microbot.pauseAllScripts) return;
+				// walk to bank until it's open then deposit everything and withdraw materials
+				if (!inventoryHasMaterialsForOneBar(config)) {
+					if (!Rs2Bank.isOpen()) {
+						Rs2Bank.walkToBankAndUseBank();
+						return;
+					}
+					Rs2Bank.depositAll();
+					if (config.SELECTED_BAR_TYPE().getId() == ItemID.IRON_BAR && Rs2Bank.hasItem(ItemID.RING_OF_FORGING) && !Rs2Equipment.isWearing(ItemID.RING_OF_FORGING)) {
+						Rs2Bank.withdrawAndEquip(ItemID.RING_OF_FORGING);
+						return;
+					}
+					withdrawRightAmountOfMaterials(config);
+					return;
+				}
 
-                // walk to bank until it's open then deposit everything and withdraw materials
-                if (!inventoryHasMaterialsForOneBar(config)) {
-                    expectingXPDrop = false;
-                    if (!Rs2Bank.isOpen()) {
-                        Rs2Bank.walkToBankAndUseBank();
-                        return;
-                    }
-                    Rs2Bank.depositAll();
-                    if (config.SELECTED_BAR_TYPE().getId() == ItemID.IRON_BAR && Rs2Bank.hasItem(ItemID.RING_OF_FORGING) && !Rs2Equipment.isWearing(ItemID.RING_OF_FORGING)) {
-                        Rs2Bank.withdrawAndEquip(ItemID.RING_OF_FORGING);
-                        return;
-                    }
-                    withdrawRightAmountOfMaterials(config);
-                    return;
-                }
+				// walk to the initial position (near furnace)
+				if (initialPlayerLocation.distanceTo(Rs2Player.getWorldLocation()) > 4) {
+					if (Rs2Bank.isOpen())
+						Rs2Bank.closeBank();
+					Rs2Walker.walkTo(initialPlayerLocation, 4);
+					return;
+				}
 
-                if (expectingXPDrop && Rs2Inventory.waitForInventoryChanges(4500)) {
-                    Rs2Antiban.actionCooldown();
-                    Rs2Antiban.takeMicroBreakByChance();
-                    return;
-                }
+				// interact with the furnace until the smelting dialogue opens in chat, click the selected bar icon
+				GameObject furnace = Rs2GameObject.findObject("furnace", true, 10, false, initialPlayerLocation);
+				if (furnace != null) {
+					Rs2GameObject.interact(furnace, "smelt");
+					Rs2Widget.sleepUntilHasWidgetText("What would you like to smelt?", 270, 5, false, 5000);
+					Rs2Widget.clickWidget(config.SELECTED_BAR_TYPE().getName());
+					Rs2Widget.sleepUntilHasNotWidgetText("What would you like to smelt?", 270, 5, false, 5000);
+					Rs2Antiban.actionCooldown();
+					Rs2Antiban.takeMicroBreakByChance();
+				}
 
-                // walk to the initial position (near furnace)
-                if (initialPlayerLocation.distanceTo(Rs2Player.getWorldLocation()) > 4) {
-                    if (Rs2Bank.isOpen())
-                        Rs2Bank.closeBank();
-                    Rs2Walker.walkTo(initialPlayerLocation, 4);
-                    return;
-                }
+			} catch (Exception ex) {
+				System.out.println(ex.getMessage());
+			}
+		}, 0, 100, TimeUnit.MILLISECONDS);
+		return true;
+	}
 
-                // interact with the furnace until the smelting dialogue opens in chat, click the selected bar icon
-                // then wait for animation to finish
-                GameObject furnace = Rs2GameObject.findObject("furnace", true, 10, false, initialPlayerLocation);
-                if (furnace != null) {
-                    Rs2GameObject.interact(furnace, "smelt");
-                    sleepUntilOnClientThread(() -> Rs2Widget.getWidget(17694733) != null);
-                    if (Rs2Widget.getWidget(17694733) != null) {
-                        Rs2Widget.clickWidget(config.SELECTED_BAR_TYPE().getName());
-                        Rs2Player.waitForAnimation();
-                        expectingXPDrop = true;
-                    }
-                }
+	@Override
+	public void shutdown() {
+		Rs2Antiban.resetAntibanSettings();
+		super.shutdown();
+	}
 
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
-        }, 0, 100, TimeUnit.MILLISECONDS);
-        return true;
-    }
+	private boolean inventoryHasMaterialsForOneBar(AutoSmeltingConfig config) {
+		for (Map.Entry<Ores, Integer> requiredMaterials : config.SELECTED_BAR_TYPE().getRequiredMaterials().entrySet()) {
+			Integer amount = requiredMaterials.getValue();
+			String name = requiredMaterials.getKey().toString();
+			if (!Rs2Inventory.hasItemAmount(name, amount)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    private boolean inventoryHasMaterialsForOneBar(AutoSmeltingConfig config) {
-        for (Map.Entry<Ores, Integer> requiredMaterials : config.SELECTED_BAR_TYPE().getRequiredMaterials().entrySet()) {
-            Integer amount = requiredMaterials.getValue();
-            String name = requiredMaterials.getKey().toString();
-            if (!Rs2Inventory.hasItemAmount(name, amount)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private void withdrawRightAmountOfMaterials(AutoSmeltingConfig config) {
-        for (Map.Entry<Ores, Integer> requiredMaterials : config.SELECTED_BAR_TYPE().getRequiredMaterials().entrySet()) {
-            Integer amountForOne = requiredMaterials.getValue();
-            String name = requiredMaterials.getKey().toString();
-            int totalAmount = config.SELECTED_BAR_TYPE().maxBarsForFullInventory() * amountForOne;
-            if (!Rs2Bank.hasBankItem(name, totalAmount)) {
-                Microbot.showMessage(MessageFormat.format("Required Materials not in bank. You need {1} {0}.", name, totalAmount));
-                super.shutdown();
-            }
-            Rs2Bank.withdrawX(name, totalAmount);
-            sleepUntil(() -> Rs2Inventory.hasItemAmount(name, totalAmount), 3500);
+	private void withdrawRightAmountOfMaterials(AutoSmeltingConfig config) {
+		for (Map.Entry<Ores, Integer> requiredMaterials : config.SELECTED_BAR_TYPE().getRequiredMaterials().entrySet()) {
+			Integer amountForOne = requiredMaterials.getValue();
+			String name = requiredMaterials.getKey().toString();
+			int totalAmount = config.SELECTED_BAR_TYPE().maxBarsForFullInventory() * amountForOne;
+			if (!Rs2Bank.hasBankItem(name, totalAmount)) {
+				Microbot.showMessage(MessageFormat.format("Required Materials not in bank. You need {1} {0}.", name, totalAmount));
+				super.shutdown();
+			}
+			Rs2Bank.withdrawX(name, totalAmount);
+			sleepUntil(() -> Rs2Inventory.hasItemAmount(name, totalAmount), 3500);
 
-            // Exit if we did not end up finding it.
-            if (!Rs2Inventory.hasItemAmount(name, totalAmount)) {
-                Microbot.showMessage("Could not find item in bank.");
-                shutdown();
-            }
-        }
-    }
+			// Exit if we did not end up finding it.
+			if (!Rs2Inventory.hasItemAmount(name, totalAmount)) {
+				Microbot.showMessage("Could not find item in bank.");
+				shutdown();
+			}
+		}
+	}
 }
