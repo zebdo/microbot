4402b572f68f0401ba4c52db9ea02fcb519f66ab
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2024-12-28 06:22:06 +0100

    Make boss timer and respawn timer classes public.
    Improved accessibility by changing `Boss` and `RespawnTimer` classes from package-private to public. This enables better extensibility and integration with other plugins or external functionalities that rely on boss timer data.

--- runelite-client/src/main/java/net/runelite/client/plugins/bosstimer/Boss.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/bosstimer/Boss.java
@@ -26,17 +26,18 @@
 package net.runelite.client.plugins.bosstimer;
 
 import com.google.common.collect.ImmutableMap;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
-import java.util.Map;
 import lombok.Getter;
 import net.runelite.api.ItemID;
 import net.runelite.api.NpcID;
 import net.runelite.client.util.RSTimeUnit;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Map;
+
 @Getter
-enum Boss
+public enum Boss
 {
 	GENERAL_GRAARDOR(NpcID.GENERAL_GRAARDOR, 90, ChronoUnit.SECONDS, ItemID.PET_GENERAL_GRAARDOR),
 	KRIL_TSUTSAROTH(NpcID.KRIL_TSUTSAROTH, 90, ChronoUnit.SECONDS, ItemID.PET_KRIL_TSUTSAROTH),
