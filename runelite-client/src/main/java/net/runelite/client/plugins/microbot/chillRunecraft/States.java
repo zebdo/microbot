fc3b34b16b8c8b919981b75426f458ae06074467
ChillX <zane.smart1010@gmail.com>
2025-01-12 14:02:48 +1100

    Added ChillX F2P Runecrafter V0.0.2

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/chillRunecraft/States.java
@@ -0,0 +1,11 @@
+package net.runelite.client.plugins.microbot.chillRunecraft;
+
+public enum States
+{
+    IDLE,
+    WALKING_TO_ALTAR,
+    BANKING,
+    CRAFTING_RUNES,
+    ENTERING_ALTAR,
+    EXITING_ALTAR
+}
