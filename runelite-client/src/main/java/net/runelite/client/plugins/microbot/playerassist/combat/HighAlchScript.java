74f4133940db3f7f6d90e0c324b3bed37b553cea
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2025-01-18 12:45:24 +0100

    - **HighAlchScript**: Add XP drop wait for alchemy automation.
      Enhanced the automation process by adding a check to wait for an XP drop after casting High Level Alchemy. This ensures better synchronization and avoids potential errors due to premature actions during alching. Removed unnecessary `MagicAction` import to clean up code dependencies.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/playerassist/combat/HighAlchScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/playerassist/combat/HighAlchScript.java
@@ -1,12 +1,13 @@
 package net.runelite.client.plugins.microbot.playerassist.combat;
 
+import net.runelite.api.Skill;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
 import net.runelite.client.plugins.microbot.playerassist.PlayerAssistConfig;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Item;
 import net.runelite.client.plugins.microbot.util.magic.Rs2Magic;
-import net.runelite.client.plugins.skillcalculator.skills.MagicAction;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,8 +17,9 @@ public boolean run(PlayerAssistConfig config) {
         try {
             if (!Microbot.isLoggedIn() || !super.run() || !config.toggleHighAlchProfitable()) return;
             Rs2Item item = Rs2Inventory.get(Rs2Item::isHaProfitable);
-            if (item != null && Rs2Magic.canCast(MagicAction.HIGH_LEVEL_ALCHEMY)) {
+            if (item != null) {
                 Rs2Magic.alch(item);
+                Rs2Player.waitForXpDrop(Skill.MAGIC, 10000, false);
             }
 
         } catch(Exception ex) {
