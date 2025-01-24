5f16412c81297cf661c6eb27453566308b7c9887
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-01-24 12:21:27 -0500

    fix: inverse condition to return false if event is executed

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/Script.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/Script.java
@@ -175,7 +175,7 @@ public abstract class Script implements IScript {
                 if (!Microbot.getBlockingEventManager().getBlockingEvents().isEmpty()) {
                     for (BlockingEvent blockingEvent : Microbot.getBlockingEventManager().getBlockingEvents()) {
                         if (blockingEvent.validate()) {
-                            return blockingEvent.execute();
+                            return !blockingEvent.execute();
                         }
                     }
                 }
