3e34e8f2b68085b8b3961ce15504ed05db863e1d
pkcs21 <drpw27@gmail.com>
2025-03-05 15:36:01 -0800

    Update FishingTrawlerConfig.java

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/fishing/FishingTrawler/FishingTrawlerConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/fishing/FishingTrawler/FishingTrawlerConfig.java
@@ -3,7 +3,6 @@ package net.runelite.client.plugins.microbot.fishing.FishingTrawler;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
-import net.runelite.client.plugins.microbot.moons.enums.State;
 
 @ConfigGroup("FishingTrawler")
 public interface FishingTrawlerConfig extends Config {
