b07f3dc36395cd5bd76f7effcc51f38f3bf91864
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-03-13 23:06:28 +0100

    Added energy potion support and updated some deprecated functions and fixed some minor bug in equality comparison

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/roguesden/RoguesDenConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/roguesden/RoguesDenConfig.java
@@ -5,6 +5,6 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigInformation;
 
 @ConfigGroup("RoguesDen")
-@ConfigInformation("Start at the rogues den. Stamina supported and recommended!")
+@ConfigInformation("Start at the rogues den. Stamina & energy potion supported and recommended!")
 public interface RoguesDenConfig extends Config {
 }
