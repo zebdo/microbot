7872f2764852319a819d70a613bc620564ba2d80
FireStarter-Instigator <davidgatward27@gmail.com>
2025-03-15 22:27:20 +0100

    Burn Baking Refactored into AUto Cooking

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/cooking/AutoCookingPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/cooking/AutoCookingPlugin.java
@@ -2,17 +2,14 @@ package net.runelite.client.plugins.microbot.cooking;
 
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.AnimationID;
 import net.runelite.api.Client;
-import net.runelite.api.Player;
-import net.runelite.api.events.AnimationChanged;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.cooking.scripts.AutoCookingScript;
+import net.runelite.client.plugins.microbot.cooking.scripts.BurnBakingScript;
 import net.runelite.client.plugins.microbot.util.mouse.VirtualMouse;
 import net.runelite.client.ui.overlay.OverlayManager;
 
@@ -27,9 +24,11 @@ import java.awt.*;
 )
 @Slf4j
 public class AutoCookingPlugin extends Plugin {
-    public static double version = 1.0;
+    public static double version = 1.1;
     @Inject
     AutoCookingScript autoCookingScript;
+    @Inject
+    BurnBakingScript burnBakingScript;
     @Inject
     private AutoCookingConfig config;
     @Inject
@@ -58,6 +57,8 @@ public class AutoCookingPlugin extends Plugin {
         switch (config.cookingActivity()) {
             case COOKING:
                 autoCookingScript.run(config);
+            case BURN_BAKING:
+                burnBakingScript.run(config);
             default:
                 Microbot.log("Invalid Cooking Activity");
         }
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/cooking/enums/CookingActivity.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/cooking/enums/CookingActivity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum CookingActivity {
     COOKING,
+    BURN_BAKING
 //    REFILLING,
 //    WINE,
 //    BAKING,
