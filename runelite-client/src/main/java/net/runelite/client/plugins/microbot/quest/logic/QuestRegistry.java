8eb02cd08c11142ba9b3e61533ec0f49cd1e93a6
chsami <sami.chkhachkhi@gmail.com>
2025-03-24 06:31:23 +0100

    feat: implement custom quest logic for Romeo and Juliet and Rune Mysteries

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/quest/logic/QuestRegistry.java
@@ -0,0 +1,30 @@
+package net.runelite.client.plugins.microbot.quest.logic;
+
+import net.runelite.api.Quest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Registry for all quests
+ */
+public class QuestRegistry {
+    private static final Map<Integer, IQuest> QUEST_MAP = new HashMap<>();
+
+    static {
+        QUEST_MAP.put(Quest.RUNE_MYSTERIES.getId(), new RuneMysteries());
+    }
+
+    /**
+     * Get the quest implementation for the given quest id
+     * @param questId
+     * @return
+     */
+    public static IQuest getQuest(int questId) {
+        IQuest quest = QUEST_MAP.getOrDefault(questId, null);
+        if (quest == null) {
+            throw new IllegalArgumentException("No implementation for " + questId);
+        }
+        return quest;
+    }
+}
