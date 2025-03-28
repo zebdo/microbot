be5108035dd9a42017367237589bd07cbc2df39e
FireStarter-Instigator <davidgatward27@gmail.com>
2025-03-28 15:52:39 +0100

    90% of issue can be dealt with in Rs2Bank class

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/grounditem/Rs2GroundItem.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/grounditem/Rs2GroundItem.java
@@ -195,8 +195,12 @@ public class Rs2GroundItem {
 
     public static RS2Item[] getAllFromWorldPoint(int range, WorldPoint worldPoint) {
         List<RS2Item> temp = new ArrayList<>();
-        int safespotX = worldPoint.getX();
-        int safespotY = worldPoint.getY();
+
+        // Convert WorldPoint to LocalPoint for sorting
+        LocalPoint safespotLocalPoint = LocalPoint.fromWorld(Microbot.getClient(), worldPoint);
+
+        int safespotX = safespotLocalPoint.getX();
+        int safespotY = safespotLocalPoint.getY();
 
         int minX = safespotX - range, minY = safespotY - range;
         int maxX = safespotX + range, maxY = safespotY + range;
@@ -215,10 +219,10 @@ public class Rs2GroundItem {
             }
         }
 
-        // Sort items based on distance from the safespot
+        // Sort items based on distance from the safespot local point
         temp = temp.stream()
                 .sorted(Comparator.comparingInt(value ->
-                        value.getTile().getLocalLocation().distanceTo(new LocalPoint(safespotX, safespotY))))
+                        value.getTile().getLocalLocation().distanceTo(safespotLocalPoint)))
                 .collect(Collectors.toList());
 
         return temp.toArray(new RS2Item[temp.size()]);
