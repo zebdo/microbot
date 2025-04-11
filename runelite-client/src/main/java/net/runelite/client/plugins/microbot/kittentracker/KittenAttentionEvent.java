e56e3fb90ff890c7e84d735c72d88840247302dd
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2025-04-11 20:22:33 +0200

    Enhance kitten interaction by replacing item usage with dialogue options

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/kittentracker/KittenAttentionEvent.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/kittentracker/KittenAttentionEvent.java
@@ -1,10 +1,9 @@
 package net.runelite.client.plugins.microbot.kittentracker;
 
-import net.runelite.api.ItemID;
 import net.runelite.client.plugins.microbot.BlockingEvent;
 import net.runelite.client.plugins.microbot.BlockingEventPriority;
 import net.runelite.client.plugins.microbot.util.Global;
-import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
+import net.runelite.client.plugins.microbot.util.dialogues.Rs2Dialogue;
 import net.runelite.client.plugins.microbot.util.npc.Rs2Npc;
 
 import javax.inject.Inject;
@@ -21,14 +20,16 @@ public class KittenAttentionEvent implements BlockingEvent
     @Override
     public boolean validate()
     {
-        return Rs2Inventory.contains(ItemID.BALL_OF_WOOL)
-                && (KittenPlugin.ATTENTION_FIRST_WARNING_TIME_LEFT_IN_SECONDS * 1000) >= kittenPlugin.getTimeBeforeNeedingAttention();
+        return (KittenPlugin.ATTENTION_FIRST_WARNING_TIME_LEFT_IN_SECONDS * 1000) >= kittenPlugin.getTimeBeforeNeedingAttention();
     }
 
     @Override
     public boolean execute()
     {
-        Rs2Npc.getNpcs("Kitten").findFirst().ifPresent(kitten -> Rs2Inventory.useItemOnNpc(ItemID.BALL_OF_WOOL, kitten));
+        Rs2Npc.getNpcs("Kitten").findFirst().ifPresent(kitten -> Rs2Npc.interact(kitten, "Interact"));
+        if (Rs2Dialogue.sleepUntilHasDialogueOption("Stroke")) {
+            Rs2Dialogue.clickOption("Stroke");
+        }
         Global.sleepUntil(() -> (KittenPlugin.ATTENTION_FIRST_WARNING_TIME_LEFT_IN_SECONDS * 1000) < kittenPlugin.getTimeBeforeNeedingAttention(),10000);
         return true;
     }
