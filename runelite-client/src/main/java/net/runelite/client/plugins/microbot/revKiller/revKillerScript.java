1c1c4a98cea1fbb06bf2a5249e239f3c2202456c
Gage307 <gagem44@gmail.com>
2025-05-03 00:31:49 -0400

    decreased pker detection delay

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/revKiller/revKillerScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/revKiller/revKillerScript.java
@@ -395,7 +395,7 @@ public class revKillerScript extends Script {
         checkForPKerFuture = scheduledExecutorService.scheduleWithFixedDelay(
                 this::futurePKCheck,
                 0,
-                500,
+                250,
                 TimeUnit.MILLISECONDS
         );
     }
