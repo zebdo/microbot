8d18ce4a28f6ad01c0d0cc3ce60ec275dd5d357f
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-05-04 18:03:09 +0200

    Initial implementation of an optimal Tears of Guthix script

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/TearsOfGuthix/TearsOfGuthixScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/TearsOfGuthix/TearsOfGuthixScript.java
@@ -92,6 +92,7 @@ public class TearsOfGuthixScript extends Script {
                         } else {
                             Microbot.log("Travelling to Tears of Guthix...");
                             Rs2Walker.walkTo(junaLocation);
+
                             if (Rs2Player.distanceTo(junaLocation) <= 5) {
                                 BOT_STATUS = State.GETTING_WORLD;
                             }
