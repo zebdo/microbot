3e5ec17d05cf9fa79a2eb95b69e216be84eb075f
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-05-05 20:20:58 +0200

    Update DriftNetScript.java
    Bugfix for driftnet hunting

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/driftnet/DriftNetScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/driftnet/DriftNetScript.java
@@ -91,12 +91,13 @@ public class DriftNetScript extends Script {
      * Increments netFetchAttempts if no nets were found
      */
     private void fetchNetsFromAnnette() {
-        Microbot.getClient().getWeight();
+        final int maxWeight = 25; // https://oldschool.runescape.wiki/w/Drift_net_fishing
+        var maxDriftnets = maxWeight - Microbot.getClient().getWeight() - 1; // Driftnets are 1kg each; doing - 1 to be safe
         Rs2GameObject.interact(ObjectID.ANNETTE, "Nets");
         sleepUntil(() -> Rs2Widget.getWidget(20250629) != null);
         Rs2Widget.clickWidgetFast(Rs2Widget.getWidget(20250629), 0, 3);
         sleepGaussian(1500, 300);
-        Rs2Keyboard.typeString("22");
+        Rs2Keyboard.typeString(String.valueOf(maxDriftnets));
         sleepGaussian(1500, 300);
         Rs2Keyboard.keyPress(KeyEvent.VK_ENTER);
         sleepGaussian(1500, 300);
