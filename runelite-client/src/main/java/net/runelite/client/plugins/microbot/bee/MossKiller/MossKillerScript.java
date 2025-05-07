9ae037d8ac8a53c0292a60601c4d45b44fdd0b02
FireStarter-Instigator <davidgatward27@gmail.com>
2025-05-07 21:08:09 +0200

    changes to moarShutDown, waiting for confirmation on how to use isPluginEnabled in order to block out if Moss Killer is scheduled the turning off of autologin and breakhandler which is native to MossKillerSCript once goals reached or consumables depleted

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/bee/MossKiller/MossKillerScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/bee/MossKiller/MossKillerScript.java
@@ -1,14 +1,14 @@
 package net.runelite.client.plugins.microbot.bee.MossKiller;
 
+import com.google.inject.Inject;
 import net.runelite.api.Skill;
 import net.runelite.api.coords.WorldPoint;
-import net.runelite.client.plugins.PluginInstantiationException;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.accountselector.AutoLoginPlugin;
 import net.runelite.client.plugins.microbot.bee.MossKiller.Enums.MossKillerState;
 import net.runelite.client.plugins.microbot.breakhandler.BreakHandlerPlugin;
 import net.runelite.client.plugins.microbot.breakhandler.BreakHandlerScript;
-import net.runelite.client.plugins.microbot.pluginscheduler.api.SchedulablePlugin;
 import net.runelite.client.plugins.microbot.util.antiban.Rs2Antiban;
 import net.runelite.client.plugins.microbot.util.antiban.Rs2AntibanSettings;
 import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
@@ -31,7 +31,6 @@ import net.runelite.client.plugins.microbot.util.security.Login;
 import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 import net.runelite.client.plugins.skillcalculator.skills.MagicAction;
 
-import javax.inject.Inject;
 import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -54,8 +53,13 @@ public class MossKillerScript extends Script {
     public int playerCounter = 0;
     public boolean bossMode = false;
 
+
+    private final MossKillerPlugin plugin;
+
     @Inject
-    SchedulablePlugin schedulablePlugin;
+    public MossKillerScript (MossKillerPlugin plugin) {
+        this.plugin = plugin;
+    }
 
     public final WorldPoint SEWER_ENTRANCE = new WorldPoint(3237, 3459, 0);
     public final WorldPoint SEWER_LADDER = new WorldPoint(3237, 9859, 0);
@@ -195,23 +199,16 @@ public class MossKillerScript extends Script {
 
     public void moarShutDown() {
         Microbot.log("super shutdown triggered");
-        varrockTeleport();
+        if(Rs2Inventory.containsAll(AIR_RUNE, FIRE_RUNE, LAW_RUNE)){
+            Rs2Magic.cast(MagicAction.VARROCK_TELEPORT);}
         //static sleep to wait till out of combat
         sleep(10000);
-        //turn off breakhandler
-        if (!Microbot.isPluginEnabled(schedulablePlugin.getClass()))
-        {stopBreakHandlerPlugin();
-        //turn off autologin and all other scripts in 5 seconds
-        Microbot.getClientThread().runOnSeperateThread(() -> {
-            if (!Microbot.pauseAllScripts) {
-                sleep(5000);
-                Microbot.pauseAllScripts = true;
-            }
-            return null;
-        });
-        Rs2Player.logout();}
+            stopBreakHandlerPlugin();
+            stopAutologin();
         sleep(1000);
-        schedulablePlugin.reportFinished("lacking teleports or consumables or have reached desired combat skill level)", false);
+        plugin.reportFinished("lacking teleports or consumables or have reached desired combat skill level)", false);
+        Microbot.log("calling script shutdown");
+        Rs2Player.logout();
         shutdown();
     }
 
@@ -263,17 +260,46 @@ public class MossKillerScript extends Script {
             return false;
         }
 
-        try {
-            // Stop the BreakHandlerPlugin
-            Microbot.getPluginManager().stopPlugin(breakHandlerPlugin);
-            Microbot.getPluginManager().setPluginEnabled(breakHandlerPlugin, false);
-            System.out.println("BreakHandlerPlugin successfully stopped.");
-            return true;
-        } catch (PluginInstantiationException e) {
-            System.err.println("Failed to stop BreakHandlerPlugin: " + e.getMessage());
-            throw new RuntimeException("An error occurred while stopping BreakHandlerPlugin", e);
-        }
+        Microbot.getClientThread().invokeLater(() -> {
+            try {
+                Microbot.getPluginManager().setPluginEnabled(breakHandlerPlugin, false);
+                Microbot.stopPlugin(breakHandlerPlugin);
+            } catch (Exception e) {
+                Microbot.log("Error stopping plugin", e);
+            }
+        });
+        return true;
     }
+
+    /**
+     * Stops the BreakHandlerPlugin if it's currently active.
+     *
+     * @return true if the plugin was successfully stopped, false if it was not found or not active.
+     */
+    public static boolean stopAutologin() {
+        // Attempt to retrieve the BreakHandlerPlugin from the active plugin list
+        AutoLoginPlugin autoLoginPlugin = (AutoLoginPlugin) Microbot.getPluginManager().getPlugins().stream()
+                .filter(plugin -> plugin.getClass().getName().equals(AutoLoginPlugin.class.getName()))
+                .findFirst()
+                .orElse(null);
+
+        // Check if the plugin was found
+        if (autoLoginPlugin == null) {
+            System.out.println("BreakHandlerPlugin not found or not running.");
+            return false;
+        }
+
+        Microbot.getClientThread().invokeLater(() -> {
+            try {
+                Microbot.getPluginManager().setPluginEnabled(autoLoginPlugin, false);
+                Microbot.stopPlugin(autoLoginPlugin);
+            } catch (Exception e) {
+                Microbot.log("Error stopping plugin", e);
+            }
+        });
+        return true;
+    }
+
 
     public void handleMossGiants() {
 
