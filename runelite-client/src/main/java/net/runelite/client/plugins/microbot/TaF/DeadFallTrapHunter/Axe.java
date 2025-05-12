81b254af9b2f1d36c0b573337fbc481104ce4d9c
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-05-12 20:36:14 +0200

    Implement deadfall hunting script

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/DeadFallTrapHunter/Axe.java
@@ -0,0 +1,40 @@
+package net.runelite.client.plugins.microbot.TaF.DeadFallTrapHunter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.runelite.api.ItemID;
+import net.runelite.api.Skill;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+
+@Getter
+@AllArgsConstructor
+public enum Axe {
+
+    BRONZE_AXE("bronze axe", ItemID.BRONZE_AXE, 1, 1),
+    BRONZE_FELLING_AXE("bronze felling axe", ItemID.BRONZE_FELLING_AXE, 1, 1),
+    IRON_AXE("iron axe", ItemID.IRON_AXE, 1, 1),
+    IRON_FELLING_AXE("iron felling axe", ItemID.IRON_FELLING_AXE, 1, 1),
+    STEEL_AXE("steel felling axe", ItemID.STEEL_FELLING_AXE, 6, 5),
+    STEEL_FELLING_AXE("steel axe", ItemID.STEEL_AXE, 6, 5),
+    BLACK_FELLING_AXE("black felling axe", ItemID.BLACK_FELLING_AXE, 11, 10),
+    BLACK_AXE("black axe", ItemID.BLACK_AXE, 11, 10),
+    MITHRIL_AXE("mithril axe", ItemID.MITHRIL_AXE, 21, 20),
+    MITHRIL_FELLING_AXE("mithril felling axe", ItemID.MITHRIL_FELLING_AXE, 21, 20),
+    ADAMANT_AXE("adamant axe", ItemID.ADAMANT_AXE, 31, 30),
+    ADAMANT_FELLING_AXE("adamant felling axe", ItemID.ADAMANT_FELLING_AXE, 31, 30),
+    RUNE_AXE("rune axe", ItemID.RUNE_AXE, 41, 40),
+    RUNE_FELLING_AXE("rune felling axe", ItemID.RUNE_FELLING_AXE, 41, 40),
+    DRAGON_AXE("dragon axe", ItemID.DRAGON_AXE, 61, 60),
+    DRAGON_FELLING_AXE("dragon felling axe", ItemID.DRAGON_FELLING_AXE, 61, 60),
+    CRYSTAL_AXE("crystal axe", ItemID.CRYSTAL_AXE, 71, 70),
+    CRYSTAL_FELLING_AXE("crystal felling axe", ItemID.CRYSTAL_FELLING_AXE, 71, 70);
+
+    private final String itemName;
+    private final int itemID;
+    private final int woodcuttingLevel;
+    private final int attackLevel;
+
+    public boolean hasRequirements(boolean axeInInventory) {
+        return Rs2Player.getSkillRequirement(Skill.WOODCUTTING, this.woodcuttingLevel) && (Rs2Player.getSkillRequirement(Skill.ATTACK, this.attackLevel) || axeInInventory);
+    }
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/DeadFallTrapHunter/DeadFallTrapInventoryHandlerScript.java
@@ -0,0 +1,51 @@
+package net.runelite.client.plugins.microbot.TaF.DeadFallTrapHunter;
+
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
+import net.runelite.client.plugins.microbot.util.inventory.Rs2ItemModel;
+import net.runelite.client.plugins.microbot.util.math.Rs2Random;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+
+import java.util.concurrent.TimeUnit;
+
+public class DeadFallTrapInventoryHandlerScript extends Script {
+    private DeadFallTrapHunterConfig config;
+    private DeadFallTrapHunterScript script;
+
+    public boolean run(DeadFallTrapHunterConfig config, DeadFallTrapHunterScript script) {
+        this.config = config;
+        this.script = script;
+        mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
+            try {
+                if (!super.run()) return;
+                if (!Microbot.isLoggedIn()) return;
+                if (!this.isRunning()) return;
+                if (!script.isRunning()) return;
+                if (!script.forceDrop) {
+                    if (Rs2Player.isMoving()) return;
+                    if (Rs2Player.isAnimating()) return;
+                    if (Rs2Inventory.size() > Rs2Random.between(8, 28)) {
+                        cleanInventory(config);
+                    }
+                    return;
+                }
+                cleanInventory(config);
+            } catch (Exception ex) {
+                System.out.println("DeadFallTrapInventoryHandlerScript: " + ex.getMessage());
+                ex.printStackTrace();
+            }
+        }, 0, 5000, TimeUnit.MILLISECONDS);
+        return true;
+    }
+
+    private void cleanInventory(DeadFallTrapHunterConfig config) {
+        for (Rs2ItemModel item : Rs2Inventory.items()) {
+            if (config.deadFallTrapHunting().getItemsToDrop().contains(item.getId())) {
+                Rs2Inventory.interact(item, "Drop");
+                sleep(600, 1200);
+            }
+        }
+        script.forceDrop = false;
+    }
+}
