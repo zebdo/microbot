ea0d478ba42685f017e6891257e89f0c41caf4f8
FireStarter-Instigator <davidgatward27@gmail.com>
2025-05-20 23:02:07 +0200

    burn baking opens bank if empty inventory at end of prepareBread block

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/cooking/scripts/BurnBakingScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/cooking/scripts/BurnBakingScript.java
@@ -400,7 +400,7 @@ public class BurnBakingScript extends Script {
                         System.out.println("Missing ingredients in the bank for bread-making.");
                         Microbot.log("Could not find flour or water in the bank for bread-making.");
                     }
-                }
+                } else if (!Rs2Bank.isOpen() && Rs2Inventory.isEmpty()) {Rs2Bank.openBank();}
             }
             // Step 5: If we already have flour and water in the inventory
             else if (Rs2Inventory.hasItem(POT_OF_FLOUR) && Rs2Inventory.hasItem(BUCKET_OF_WATER)) {
