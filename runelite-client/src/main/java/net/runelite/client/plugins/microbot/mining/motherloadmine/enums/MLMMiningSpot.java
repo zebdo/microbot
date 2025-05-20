4b0fa3e40dc93ae2900fa9575a12db4ab8f7687e
SandalBandit <119099353+SandalBandit@users.noreply.github.com>
2025-05-20 14:51:00 +1200

    Update to mining location coordinates

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/mining/motherloadmine/enums/MLMMiningSpot.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/mining/motherloadmine/enums/MLMMiningSpot.java
@@ -14,7 +14,7 @@ public enum MLMMiningSpot {
     WEST_LOWER(Arrays.asList(new WorldPoint(3731, 5659, 0), new WorldPoint(3731, 5663, 0))),
     WEST_MID(Arrays.asList(new WorldPoint(3730, 5666, 0), new WorldPoint(3731, 5669, 0))),
     WEST_UPPER(Arrays.asList(new WorldPoint(3752, 5683, 0), new WorldPoint(3752, 5680, 0))),
-    SOUTH_EAST(Arrays.asList(new WorldPoint(3745, 5647, 0), new WorldPoint(3756, 5653, 0))),
+    SOUTH_EAST(Arrays.asList(new WorldPoint(3753, 5650, 0), new WorldPoint(3756, 5653, 0))),
     SOUTH_WEST(Arrays.asList(new WorldPoint(3740, 5648, 0), new WorldPoint(3740, 5648, 0))),
     NORTH(null),
     EAST_UPPER(Arrays.asList(new WorldPoint(3760, 5673, 0), new WorldPoint(3759, 5673, 0))),
