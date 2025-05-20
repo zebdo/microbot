405c4f97df4e4b831dc141aca7c75fb5ec7adf75
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-05-20 22:42:16 +0200

    Make schedulable

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/VolcanicAshMiner/VolcanicAshMinerPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/VolcanicAshMiner/VolcanicAshMinerPlugin.java
@@ -4,6 +4,12 @@ import com.google.inject.Provides;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.pluginscheduler.api.SchedulablePlugin;
+import net.runelite.client.plugins.microbot.pluginscheduler.condition.logical.AndCondition;
+import net.runelite.client.plugins.microbot.pluginscheduler.condition.logical.LogicalCondition;
+import net.runelite.client.plugins.microbot.pluginscheduler.event.PluginScheduleEntrySoftStopEvent;
+import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
 import net.runelite.client.plugins.microbot.util.misc.TimeUtils;
 import net.runelite.client.ui.overlay.OverlayManager;
 
@@ -17,7 +23,7 @@ import java.time.Instant;
         tags = {"Volcanic", "ash", "mining", "ironman", "taf", "microbot"},
         enabledByDefault = false
 )
-public class VolcanicAshMinerPlugin extends Plugin {
+public class VolcanicAshMinerPlugin extends Plugin implements SchedulablePlugin {
 
     private Instant scriptStartTime;
     @Inject
@@ -28,6 +34,7 @@ public class VolcanicAshMinerPlugin extends Plugin {
     private VolcanicAshMinerOverlay volcanicAshMinerOverlay;
     @Inject
     private VolcanicAshMinerScript volcanicAshMinerScript;
+    private LogicalCondition stopCondition = new AndCondition();
 
     protected String getTimeRunning() {
         return scriptStartTime != null ? TimeUtils.getFormattedDurationBetween(scriptStartTime, Instant.now()) : "";
@@ -51,5 +58,21 @@ public class VolcanicAshMinerPlugin extends Plugin {
     @Provides
     VolcanicAshMinerConfig provideConfig(ConfigManager configManager) {
         return configManager.getConfig(VolcanicAshMinerConfig.class);
+    }
+
+    @Override
+    public void onPluginScheduleEntrySoftStopEvent(PluginScheduleEntrySoftStopEvent event) {
+        if (event.getPlugin() == this) {
+            if (volcanicAshMinerScript != null) {
+                Rs2Bank.walkToBank();
+            }
+            Microbot.stopPlugin(this);
+        }
+    }
+
+    @Override
+    public LogicalCondition getStopCondition() {
+        // Create a new stop condition
+        return this.stopCondition;
     }
 }
