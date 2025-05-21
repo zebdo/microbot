caceff6137481400ccd4fd24fbce6a04071e1031
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-05-21 12:28:10 +0200

    Add GiantSeaweedFarmer

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/GiantSeaweedFarmer/GiantSeaweedFarmerOverlay.java
@@ -0,0 +1,41 @@
+package net.runelite.client.plugins.microbot.TaF.GiantSeaweedFarmer;
+
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+public class GiantSeaweedFarmerOverlay extends OverlayPanel {
+    private final GiantSeaweedFarmerPlugin plugin;
+
+    @Inject
+    GiantSeaweedFarmerOverlay(GiantSeaweedFarmerPlugin plugin) {
+        super(plugin);
+        this.plugin = plugin;
+        setPosition(OverlayPosition.TOP_LEFT);
+        setNaughty();
+    }
+
+
+    @Override
+    public Dimension render(Graphics2D graphics) {
+        try {
+            panelComponent.setPreferredSize(new Dimension(200, 300));
+            panelComponent.getChildren().add(TitleComponent.builder()
+                    .text("GiantSeaweedFarmer - v" + GiantSeaweedFarmerScript.VERSION)
+                    .color(Color.GREEN)
+                    .build());
+            panelComponent.getChildren().add(LineComponent.builder().build());
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Running: " + plugin.getTimeRunning())
+                    .leftColor(Color.WHITE)
+                    .build());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return super.render(graphics);
+    }
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/GiantSeaweedFarmer/GiantSeaweedFarmerStatus.java
@@ -0,0 +1,8 @@
+package net.runelite.client.plugins.microbot.TaF.GiantSeaweedFarmer;
+
+public enum GiantSeaweedFarmerStatus {
+    BANKING,
+    WALKING_TO_PATCH,
+    FARMING,
+    RETURN_TO_BANK
+}
