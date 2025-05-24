e2a3a13accba8f63b2029e090a952f4cb1db6eb4
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-05-23 21:52:40 -0400

    fix (aio-fighter): refactor alch script to use early returns instead of holding the thread until the next alch, we can increase the time to check if needed

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/aiofighter/combat/HighAlchScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/aiofighter/combat/HighAlchScript.java
@@ -1,63 +1,108 @@
 package net.runelite.client.plugins.microbot.aiofighter.combat;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import net.runelite.api.Skill;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
 import net.runelite.client.plugins.microbot.aiofighter.AIOFighterConfig;
+import net.runelite.client.plugins.microbot.globval.enums.InterfaceTab;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2ItemModel;
 import net.runelite.client.plugins.microbot.util.item.Rs2ExplorersRing;
 import net.runelite.client.plugins.microbot.util.keyboard.Rs2Keyboard;
 import net.runelite.client.plugins.microbot.util.magic.Rs2Magic;
+import net.runelite.client.plugins.microbot.util.magic.Rs2Spells;
+import net.runelite.client.plugins.microbot.util.math.Rs2Random;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
 import net.runelite.client.plugins.microbot.util.settings.Rs2Settings;
 import net.runelite.client.plugins.microbot.util.tabs.Rs2Tab;
 import net.runelite.client.plugins.microbot.util.widget.Rs2Widget;
-import net.runelite.client.plugins.skillcalculator.skills.MagicAction;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+public class HighAlchScript extends Script
+{
 
-public class HighAlchScript extends Script {
+	private static final int MIN_TICKS = (int) Math.ceil(30.0 / 0.6);
+	private static final int MAX_TICKS = (int) Math.floor(45.0 / 0.6);
+	private int lastAlchCheckTick = -1;
+	private int nextAlchIntervalTicks = 0;
 
-    private int timeUntilCheckHighAlch = 60000;
+	public boolean run(AIOFighterConfig config)
+	{
+		mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
+			try
+			{
+				if (!Microbot.isLoggedIn() || !super.run() || !config.toggleHighAlchProfitable())
+				{
+					return;
+				}
+				List<Rs2ItemModel> items = Rs2Inventory.getList(Rs2ItemModel::isHaProfitable);
 
-    public boolean run(AIOFighterConfig config) {
-        mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
-            try {
-                if (!Microbot.isLoggedIn() || !super.run() || !config.toggleHighAlchProfitable()) return;
-                List<Rs2ItemModel> items = Rs2Inventory.getList(Rs2ItemModel::isHaProfitable);
+				if (items.isEmpty())
+				{
+					if (Rs2Tab.getCurrentTab() != InterfaceTab.INVENTORY)
+					{
+						Rs2Tab.switchToInventoryTab();
+					}
+					return;
+				}
 
-                if (items.isEmpty()) return;
+				int currentTick = Microbot.getClient().getTickCount();
 
-                if (Rs2ExplorersRing.hasRing() && Rs2ExplorersRing.hasCharges()) {
-                    for (Rs2ItemModel item: items) {
-                        Rs2ExplorersRing.highAlch(item);
-                    }
-                    Rs2ExplorersRing.closeInterface();
-                } else  if (Rs2Magic.canCast(MagicAction.HIGH_LEVEL_ALCHEMY)) {
-                    for (Rs2ItemModel item: items) {
-                        Rs2Magic.alch(item);
-                        if (item.getHaPrice() > Rs2Settings.getMinimumItemValueAlchemyWarning()) {
-                            sleepUntil(() -> Rs2Widget.hasWidget("Proceed to cast High Alchemy on it"));
-                            if (Rs2Widget.hasWidget("Proceed to cast High Alchemy on it")) {
-                                Rs2Keyboard.keyPress('1');
-                            }
-                        }
-                    }
-                } else {
-                    Rs2Tab.switchToInventoryTab();
-                    sleep(timeUntilCheckHighAlch);
-                    timeUntilCheckHighAlch = timeUntilCheckHighAlch * 5;
-                }
+				if (lastAlchCheckTick != -1 && currentTick - lastAlchCheckTick < nextAlchIntervalTicks)
+				{
+					return;
+				}
 
-            } catch (Exception ex) {
-                Microbot.logStackTrace(this.getClass().getSimpleName(), ex);
-            }
-        }, 0, 600, TimeUnit.MILLISECONDS);
-        return true;
-    }
+				lastAlchCheckTick = currentTick;
+				nextAlchIntervalTicks = Rs2Random.nextInt(MIN_TICKS, MAX_TICKS, 1.5, true);
+
+				if (Rs2ExplorersRing.hasRing() && Rs2ExplorersRing.hasCharges())
+				{
+					for (Rs2ItemModel item : items)
+					{
+						if (!isRunning())
+						{
+							break;
+						}
+
+						Rs2ExplorersRing.highAlch(item);
+					}
+					Rs2ExplorersRing.closeInterface();
+				}
+				else if (Rs2Player.getSkillRequirement(Skill.MAGIC, Rs2Spells.HIGH_LEVEL_ALCHEMY.getRequiredLevel()) && Rs2Magic.hasRequiredRunes(Rs2Spells.HIGH_LEVEL_ALCHEMY))
+				{
+					for (Rs2ItemModel item : items)
+					{
+						if (!isRunning())
+						{
+							break;
+						}
+
+						Rs2Magic.alch(item);
+						if (item.getHaPrice() > Rs2Settings.getMinimumItemValueAlchemyWarning())
+						{
+							sleepUntil(() -> Rs2Widget.hasWidget("Proceed to cast High Alchemy on it"));
+							if (Rs2Widget.hasWidget("Proceed to cast High Alchemy on it"))
+							{
+								Rs2Keyboard.keyPress('1');
+								Rs2Player.waitForAnimation();
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Microbot.logStackTrace(this.getClass().getSimpleName(), ex);
+			}
+		}, 0, 600, TimeUnit.MILLISECONDS);
+		return true;
+	}
 
 
-    public void shutdown() {
-        super.shutdown();
-    }
+	public void shutdown()
+	{
+		super.shutdown();
+	}
 }
