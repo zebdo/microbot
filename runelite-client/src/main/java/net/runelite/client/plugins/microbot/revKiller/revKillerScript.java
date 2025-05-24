2b36b07735728fbf7cd53e129f68b8a7fa97501a
Gage307 <gagem44@gmail.com>
2025-05-24 11:06:57 -0400

    fixed issue with low level prayer accounts getting stuck at rej fount

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/revKiller/revKillerScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/revKiller/revKillerScript.java
@@ -726,7 +726,7 @@ public class revKillerScript extends Script {
                     sleepUntil(()-> !Rs2Player.isAnimating(), Rs2Random.between(1000,4000));
                 }
             }
-            if(Rs2Player.getBoostedSkillLevel(Skill.PRAYER) >= rejat && Rs2Player.getRunEnergy() >= runener){
+            if(Rs2Player.getBoostedSkillLevel(Skill.PRAYER) >= rejat || Rs2Player.getRunEnergy() >= runener){
                 break;
             }
 
