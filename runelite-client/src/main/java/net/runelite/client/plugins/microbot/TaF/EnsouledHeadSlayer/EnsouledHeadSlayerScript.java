03d559770eabfdd3f5cb461407abfd5a811adefc
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-05-25 13:38:43 +0200

    Update for calcified rocks + EnsouledHeadSlayer

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/EnsouledHeadSlayer/EnsouledHeadSlayerScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/EnsouledHeadSlayer/EnsouledHeadSlayerScript.java
@@ -2,6 +2,7 @@ package net.runelite.client.plugins.microbot.TaF.EnsouledHeadSlayer;
 
 import net.runelite.api.Skill;
 import net.runelite.api.Varbits;
+import net.runelite.api.coords.WorldArea;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
@@ -12,9 +13,11 @@ import net.runelite.client.plugins.microbot.util.antiban.Rs2AntibanSettings;
 import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
 import net.runelite.client.plugins.microbot.util.bank.enums.BankLocation;
 import net.runelite.client.plugins.microbot.util.combat.Rs2Combat;
+import net.runelite.client.plugins.microbot.util.gameobject.Rs2GameObject;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
 import net.runelite.client.plugins.microbot.util.magic.Rs2Magic;
 import net.runelite.client.plugins.microbot.util.math.Rs2Random;
+import net.runelite.client.plugins.microbot.util.misc.Rs2Food;
 import net.runelite.client.plugins.microbot.util.npc.Rs2Npc;
 import net.runelite.client.plugins.microbot.util.player.Rs2Player;
 import net.runelite.client.plugins.microbot.util.tabs.Rs2Tab;
@@ -32,9 +35,11 @@ import static net.runelite.client.plugins.microbot.util.antiban.enums.ActivityIn
 
 public class EnsouledHeadSlayerScript extends Script {
     public static final String VERSION = "1.0";
+    public static final int ENSOULED_GROUND_GRAPHICS = 1290;
     public EnsouledHeadSlayerStatus BOT_STATE = EnsouledHeadSlayerStatus.BANKING;
     private final WorldPoint ALTAR_LOCATION = new WorldPoint(1711, 3882, 0);
     private Rs2InventorySetup inventorySetup = null;
+    private final WorldArea SUMMON_AREA = new WorldArea(ALTAR_LOCATION, 25,25);
     private boolean firstRun = true;
 
     {
@@ -87,11 +92,19 @@ public class EnsouledHeadSlayerScript extends Script {
                 System.out.println("Exception message: " + ex.getMessage());
                 ex.printStackTrace();
             }
-        }, 0, 600, TimeUnit.MILLISECONDS);
+        }, 0, 1000, TimeUnit.MILLISECONDS);
         return true;
     }
 
     private void handleReanimatingAndKilling(EnsouledHeadSlayerConfig config) {
+        if (Rs2Player.getHealthPercentage() < 50) {
+            Rs2Player.eatAt(50);
+        }
+//        if (!Rs2Player.getWorldLocation().isInArea(SUMMON_AREA) && !Rs2Player.isInCombat()) {
+//            Microbot.log("Not in the summoning area, walking to altar...");
+//            BOT_STATE = EnsouledHeadSlayerStatus.WALKING_TO_ALTAR;
+//            return;
+//        }
     /*
 		0 = Standard
 		1 = Ancient
@@ -105,7 +118,7 @@ public class EnsouledHeadSlayerScript extends Script {
         }
         Rs2Combat.enableAutoRetialiate();
         var ensouledHead = Rs2Inventory.count("ensouled");
-        if (ensouledHead == 0) {
+        if (ensouledHead == 0 && !Rs2Combat.inCombat()) {
             Microbot.log("No ensouled heads found in inventory, banking...");
             BOT_STATE = EnsouledHeadSlayerStatus.BANKING;
             return;
@@ -115,11 +128,11 @@ public class EnsouledHeadSlayerScript extends Script {
             BOT_STATE = EnsouledHeadSlayerStatus.BANKING;
             return;
         }
-        if (Rs2Player.isInCombat()) {
+        if (Rs2Player.getInteracting() != null) {
             return;
         }
-        var enemy = Rs2Npc.getNpcs("Reanimated").collect(Collectors.toList());
-        if (enemy.isEmpty()) {
+        var enemy = Rs2Npc.getAttackableNpcs("Reanimated").collect(Collectors.toList());
+        if (enemy.isEmpty() && Rs2Player.getInteracting() == null && Rs2GameObject.getGroundObject(ENSOULED_GROUND_GRAPHICS) == null) {
             var ensouledHeadItem = Rs2Inventory.get("ensouled");
             var spell = Arrays.stream(EnsouledHeads.values())
                     .filter(x -> x.hasRequirements() && Objects.equals(x.getName(), ensouledHeadItem.getName()))
@@ -129,11 +142,15 @@ public class EnsouledHeadSlayerScript extends Script {
                 Rs2Magic.cast(spell.getMagicSpell());
                 sleepUntil(() -> Rs2Tab.getCurrentTab() == InterfaceTab.INVENTORY, 1000);
                 Rs2Inventory.interact(ensouledHeadItem);
-                sleepUntil(Rs2Combat::inCombat, 30000);
+                // NPC animation getting cast
+                sleepUntil(() -> Rs2GameObject.getGroundObject(ENSOULED_GROUND_GRAPHICS) != null, 4000);
+                // NPC animation finished - NPC should be spawned
+                sleepUntil(() -> Rs2GameObject.getGroundObject(ENSOULED_GROUND_GRAPHICS) == null, 4000);
+                sleep(4000,4000);
             }
         } else {
             Microbot.log("Found NPC");
-            Rs2Npc.attack(enemy.stream().findFirst().get());
+            enemy.stream().findFirst().ifPresent(Rs2Npc::attack);
         }
     }
 
@@ -153,9 +170,12 @@ public class EnsouledHeadSlayerScript extends Script {
             if (!getRunesAndEnsouledHeads(config)) {
                 Microbot.log("Failed to withdraw runes or ensouled heads");
             }
-            var foodAmount = config.foodAmount() - Rs2Inventory.count(config.food().getId();
-            Microbot.log("Withdrawing " + foodAmount + " " + config.food().getName() + " from bank");
-            Rs2Bank.withdrawX(config.food().getId(), foodAmount);
+            var foodAmount = config.foodAmount() - Rs2Inventory.count(config.food().getId());
+            if (foodAmount > 0) {
+                Microbot.log("Withdrawing " + foodAmount + " " + config.food().getName() + " from bank");
+                Rs2Bank.withdrawX(config.food().getId(), foodAmount);
+            }
+
             Rs2Bank.closeBank();
         }
 
@@ -204,7 +224,7 @@ public class EnsouledHeadSlayerScript extends Script {
             }
 
             // Calculate available slots
-            int availableSlots = Rs2Inventory.emptySlotCount() - config.foodAmount() - 3;
+            int availableSlots = Rs2Inventory.getEmptySlots() - config.foodAmount() - 3;
 
             if (availableSlots <= 0) {
                 Microbot.log("Not enough inventory space");
@@ -242,10 +262,13 @@ public class EnsouledHeadSlayerScript extends Script {
             withdrawRunesForSpell(selectedHead.getMagicSpell());
             if (config.useGamesNecklaceForBanking() && Rs2Inventory.count("Games necklace") < 1) {
                 Rs2Bank.withdrawX("Games necklace", 1);
+                sleep(600, 800);
             }
             if (config.useArcheusLibraryTeleport() && Rs2Inventory.count("earth rune") < 2 && Rs2Inventory.count("law rune") < 1) {
-                Rs2Bank.withdrawX("earth rune", Rs2Random.between(2,10));
-                Rs2Bank.withdrawX("law rune", Rs2Random.between(1,10));
+                Rs2Bank.withdrawX("earth rune", Rs2Random.between(4,10));
+                sleep(600, 800);
+                Rs2Bank.withdrawX("law rune", Rs2Random.between(4,10));
+                sleep(600, 800);
             }
 
             int headCount = Rs2Bank.count(selectedHead.getItemId());
@@ -253,7 +276,7 @@ public class EnsouledHeadSlayerScript extends Script {
                 Microbot.log("No " + selectedHead.getName() + " found in bank");
                 return false;
             }
-            var headsToWithdraw = Rs2Inventory.emptySlotCount() - config.foodAmount();
+            var headsToWithdraw = Rs2Inventory.getEmptySlots() - config.foodAmount();
             Microbot.log("Withdrawing " + headsToWithdraw + " " + selectedHead.getName() + " from bank");
             Rs2Bank.withdrawX(selectedHead.getName(), headsToWithdraw);
         }
