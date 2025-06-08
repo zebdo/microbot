b5ef853edd6295ca9574eed884947080b87db4e7
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-06-08 01:47:06 -0400

    chore (refactor): rs2keyboard

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/keyboard/Rs2Keyboard.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/keyboard/Rs2Keyboard.java
@@ -7,156 +7,110 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 import static java.awt.event.KeyEvent.CHAR_UNDEFINED;
-import static net.runelite.client.plugins.microbot.util.math.Random.random;
+import net.runelite.client.plugins.microbot.util.math.Rs2Random;
 
-public class Rs2Keyboard {
+public class Rs2Keyboard
+{
 
-    public static Canvas getCanvas() {
-        return Microbot.getClient().getCanvas();
-    }
+	private static Canvas getCanvas()
+	{
+		return Microbot.getClient().getCanvas();
+	}
 
-    public static void typeString(final String word) {
-        boolean originalFocusValue = Microbot.getClient().getCanvas().isFocusable();
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(!originalFocusValue);
-        }
+	private static void withFocusCanvas(Runnable action)
+	{
+		Canvas canvas = getCanvas();
+		boolean originalFocus = canvas.isFocusable();
+		if (!originalFocus) canvas.setFocusable(true);
 
-        for (int i = 0; i < word.length(); i++) {
-            final int randomizer = random(20, 200);
+		try
+		{
+			action.run();
+		}
+		finally
+		{
+			if (!originalFocus) canvas.setFocusable(false);
+		}
+	}
 
-            KeyEvent keyEvent = new KeyEvent(getCanvas(), KeyEvent.KEY_TYPED, System.currentTimeMillis() + randomizer, 0, KeyEvent.VK_UNDEFINED, word.charAt(i));
+	private static void dispatchKeyEvent(int id, int keyCode, char keyChar, int delay)
+	{
+		KeyEvent event = new KeyEvent(
+			getCanvas(),
+			id,
+			System.currentTimeMillis() + delay,
+			0,
+			keyCode,
+			keyChar
+		);
+		getCanvas().dispatchEvent(event);
+	}
 
-            Microbot.getClientThread().runOnClientThreadOptional(() -> {
-                getCanvas().dispatchEvent(keyEvent);
-                return true;
-            });
+	public static void typeString(final String word)
+	{
+		withFocusCanvas(() -> {
+			for (char c : word.toCharArray())
+			{
+				int delay = Rs2Random.between(20, 200);
+				dispatchKeyEvent(KeyEvent.KEY_TYPED, KeyEvent.VK_UNDEFINED, c, delay);
+				Global.sleep(100, 200);
+			}
+		});
+	}
 
-            Global.sleep(100, 200);
-        }
+	public static void keyPress(final char key)
+	{
+		withFocusCanvas(() -> {
+			int delay = Rs2Random.between(20, 200);
+			dispatchKeyEvent(KeyEvent.KEY_TYPED, KeyEvent.VK_UNDEFINED, key, delay);
+		});
+	}
 
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(originalFocusValue);
-        }
-    }
+	public static void holdShift()
+	{
+		withFocusCanvas(() -> {
+			int delay = Rs2Random.between(20, 200);
+			dispatchKeyEvent(KeyEvent.KEY_PRESSED, KeyEvent.VK_SHIFT, CHAR_UNDEFINED, delay);
+		});
+	}
 
+	public static void releaseShift()
+	{
+		withFocusCanvas(() -> {
+			int delay = Rs2Random.between(20, 200);
+			dispatchKeyEvent(KeyEvent.KEY_RELEASED, KeyEvent.VK_SHIFT, CHAR_UNDEFINED, delay);
+		});
+	}
 
-    public static void keyPress(final char key) {
-        boolean originalFocusValue = Microbot.getClient().getCanvas().isFocusable();
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(!originalFocusValue);
-        }
+	public static void keyHold(int key)
+	{
+		withFocusCanvas(() ->
+			dispatchKeyEvent(KeyEvent.KEY_PRESSED, key, CHAR_UNDEFINED, 0)
+		);
+	}
 
-        final int randomizer = random(20, 200);
+	public static void keyRelease(int key)
+	{
+		withFocusCanvas(() -> {
+			int delay = Rs2Random.between(20, 200);
+			dispatchKeyEvent(KeyEvent.KEY_RELEASED, key, CHAR_UNDEFINED, delay);
+		});
+	}
 
-        KeyEvent keyEvent = new KeyEvent(getCanvas(), KeyEvent.KEY_TYPED, System.currentTimeMillis() + randomizer, 0, KeyEvent.VK_UNDEFINED, key);
+	public static void keyPress(int key)
+	{
+		keyHold(key);
+		keyRelease(key);
+	}
 
-        Microbot.getClientThread().runOnClientThreadOptional(() -> {
-            getCanvas().dispatchEvent(keyEvent);
-            return true;
-        });
-
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(originalFocusValue);
-        }
-
-    }
-
-    public static void holdShift() {
-        boolean originalFocusValue = Microbot.getClient().getCanvas().isFocusable();
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(!originalFocusValue);
-        }
-
-        final int randomizer = random(20, 200);
-
-        KeyEvent keyEvent = new KeyEvent(getCanvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis() + randomizer, 0, KeyEvent.VK_SHIFT, CHAR_UNDEFINED);
-
-        Microbot.getClientThread().runOnClientThreadOptional(() -> {
-            getCanvas().dispatchEvent(keyEvent);
-            return true;
-        });
-
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(originalFocusValue);
-        }
-
-    }
-
-    public static void releaseShift() {
-        boolean originalFocusValue = Microbot.getClient().getCanvas().isFocusable();
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(!originalFocusValue);
-        }
-
-        final int randomizer = random(20, 200);
-
-        KeyEvent keyEvent = new KeyEvent(getCanvas(), KeyEvent.KEY_RELEASED, System.currentTimeMillis() + randomizer, 0, KeyEvent.VK_SHIFT);
-
-        Microbot.getClientThread().runOnClientThreadOptional(() -> {
-            getCanvas().dispatchEvent(keyEvent);
-            return true;
-        });
-
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(originalFocusValue);
-        }
-
-    }
-
-    public static void keyHold(int key) {
-        boolean originalFocusValue = Microbot.getClient().getCanvas().isFocusable();
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(!originalFocusValue);
-        }
-
-        KeyEvent keyEvent = new KeyEvent(getCanvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, key);
-
-        Microbot.getClientThread().runOnClientThreadOptional(() -> {
-            getCanvas().dispatchEvent(keyEvent);
-            return true;
-        });
-
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(originalFocusValue);
-        }
-
-    }
-
-    public static void keyRelease(int key) {
-        boolean originalFocusValue = Microbot.getClient().getCanvas().isFocusable();
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(!originalFocusValue);
-        }
-
-        final int randomizer = random(20, 200);
-
-        KeyEvent keyEvent = new KeyEvent(getCanvas(), KeyEvent.KEY_RELEASED, System.currentTimeMillis() + randomizer, 0, key);
-
-        Microbot.getClientThread().runOnClientThreadOptional(() -> {
-            getCanvas().dispatchEvent(keyEvent);
-            return true;
-        });
-
-        if (!originalFocusValue) {
-            Microbot.getClient().getCanvas().setFocusable(originalFocusValue);
-        }
-
-    }
-
-    public static void keyPress(int key) {
-        keyHold(key);
-        keyRelease(key);
-    }
-
-    public static void enter() {
-        keyPress(KeyEvent.VK_ENTER);
-
-        //FIX: this is to avoid automatically login with jagex account when you are on the login screen
-        KeyEvent keyEvent = new KeyEvent(getCanvas(), KeyEvent.KEY_TYPED, System.currentTimeMillis() , 0, KeyEvent.VK_UNDEFINED, '\n');
-
-        Microbot.getClientThread().runOnClientThreadOptional(() -> {
-            getCanvas().dispatchEvent(keyEvent);
-            return true;
-        });
-    }
+	public static void enter()
+	{
+		// this is to avoid automatically login with jagex account when you are on the login screen
+		if (!Microbot.isLoggedIn()) {
+			dispatchKeyEvent(KeyEvent.KEY_TYPED, KeyEvent.VK_UNDEFINED, '\n', 0);
+			return;
+		}
+		
+		keyPress(KeyEvent.VK_ENTER);
+	}
 }
