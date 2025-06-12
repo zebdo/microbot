ef56e80ae2122695eaf7ec1a8f5190776e89ec29
FunkyMonkeyCloud <na@gmail.com>
2025-06-13 09:38:27 +1000

    While loops now removed for stability & good practice. Resupply should be functioning correctly

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/moonsOfPeril/moonsOfPerilScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/moonsOfPeril/moonsOfPerilScript.java
@@ -29,8 +29,20 @@ public class moonsOfPerilScript extends Script {
                 long startTime = System.currentTimeMillis();
 
                 // CODE STARTS HERE
-                retrieveTravelItems();
-                retrieveDeathItems();
+/*                if (retrieveTravelItems()) {
+                    Microbot.log("Successfully retrieved travel items.");
+                }
+                else {
+                    Microbot.log("Could not retrieve travel items...shutting down.");
+                    shutdown();
+                }
+                if (retrieveDeathItems()) {
+                    Microbot.log("Successfully retrieved death items.");
+                }
+                else {
+                    Microbot.log("Could not retrieve death items...shutting down.");
+                    shutdown();
+                }*/
                 if (checkSupplies(config.moonlightPotionsMinimum(), config.cookedBreamMinimum())) {
                     walkToSupplies();
                     makeMoonlightPotions(config.moonlightPotionsQuantum());
@@ -74,23 +86,24 @@ public class moonsOfPerilScript extends Script {
         Microbot.log("Need to a create " + amountToCreate + " Moonlight potions");
 
         if (amountToCreate > 0) {
-            // Take the herblore supplies from supply crate
-            while (Rs2Inventory.count(ItemID.VIAL_WATER) < amountToCreate) {
-                Microbot.log("Attempt to obtain herblore supplies from supply crate");
-                if (Rs2GameObject.interact(ObjectID.PMOON_SUPPLY_CRATE,"Take from")) {
+            /* ---------- Grab vials & pestle and mortar ---------- */
+            for (int tries = 0; Rs2Inventory.count(ItemID.VIAL_WATER) < amountToCreate && tries < 4; tries++) {
+                Microbot.log("Taking vials (" + (tries + 1) + "/4)");
+                if (Rs2GameObject.interact(ObjectID.PMOON_SUPPLY_CRATE, "Take from")) {
                     Rs2Dialogue.sleepUntilHasDialogueOption("Take herblore supplies.");
                     Rs2Dialogue.clickOption("Take herblore supplies.");
-                    if (Rs2Inventory.waitForInventoryChanges(1500)) {
-                        Microbot.log("Herblore Supplies Obtained.");
-                    }
+                    Rs2Inventory.waitForInventoryChanges(1500);
                 }
             }
-            // Forage for herblore supplies
-            while (Rs2Inventory.count(ItemID.MOONLIGHT_GRUB) + Rs2Inventory.count(ItemID.MOONLIGHT_GRUB_PASTE) < amountToCreate) {
-                Microbot.log("Attempting to collect Moonlight Grub");
-                if (Rs2GameObject.interact(ObjectID.PMOON_GRUB_SAPLING,"Collect-from")) {
-                    sleepUntil(() -> (Rs2Inventory.count(ItemID.MOONLIGHT_GRUB) + Rs2Inventory.count(ItemID.MOONLIGHT_GRUB_PASTE) > amountToCreate), 8000);
-                    Microbot.log("Moonlight Grubs obtained.");
+            /* ---------- Pick grubs ---------- */
+            for (int tries = 0; Rs2Inventory.count(ItemID.MOONLIGHT_GRUB)
+                    + Rs2Inventory.count(ItemID.MOONLIGHT_GRUB_PASTE) < amountToCreate
+                    && tries < 6; tries++) {
+                Microbot.log("Collecting grubs (" + (tries + 1) + "/6)");
+                if (Rs2GameObject.interact(ObjectID.PMOON_GRUB_SAPLING, "Collect-from")) {
+                    sleepUntil(() -> Rs2Inventory.count(ItemID.MOONLIGHT_GRUB)
+                                    + Rs2Inventory.count(ItemID.MOONLIGHT_GRUB_PASTE) >= amountToCreate,
+                            8_000);
                 }
             }
             // Process the supplies into Moonlight paste
@@ -111,20 +124,16 @@ public class moonsOfPerilScript extends Script {
             }
 
             // Drop excess herblore supplies
-            Microbot.log("Attempting to drop excess herblore supplies");
-            while (Rs2Inventory.contains(ItemID.PESTLE_AND_MORTAR)) {
-                Rs2Inventory.drop(ItemID.PESTLE_AND_MORTAR);
-                sleep(200,400);
+            for (int id : new int[]{
+                    ItemID.PESTLE_AND_MORTAR,
+                    ItemID.MOONLIGHT_GRUB,
+                    ItemID.MOONLIGHT_GRUB_PASTE}) {
+
+                while (Rs2Inventory.contains(id)) {
+                    Rs2Inventory.drop(id);
+                    sleep(200, 400);
+                }
             }
-            while (Rs2Inventory.contains(ItemID.MOONLIGHT_GRUB)) {
-                Rs2Inventory.drop(ItemID.MOONLIGHT_GRUB);
-                sleep(200,400);
-            }
-            while (Rs2Inventory.contains(ItemID.MOONLIGHT_GRUB_PASTE)) {
-                Rs2Inventory.drop(ItemID.MOONLIGHT_GRUB_PASTE);
-                sleep(200,400);
-            }
-            Microbot.log("All excess herblore supplies should now be dropped");
         }
 
     }
@@ -147,36 +156,29 @@ public class moonsOfPerilScript extends Script {
         }
         Microbot.log("Walking to fishing spot");
         Rs2Walker.walkFastCanvas(new WorldPoint(1520,9689,0));
-        while (!Rs2Inventory.isFull()) {
+        /* One click starts fishing; wait up to 3 min for the bag to fill.
+        If animation stops early, give it up to four more nudges.        */
+        for (int nudges = 0; !Rs2Inventory.isFull() && nudges < 5; nudges++) {
             if (!Rs2Player.isAnimating()) {
-                Rs2GameObject.interact(51367, "Fish"); // restart if animation stopped
-                sleep(3000, 4000);
+                Rs2GameObject.interact(51367, "Fish");
+                sleep(600, 900);
             }
-            sleep(300, 500);
+            sleepUntil(Rs2Inventory::isFull, 5_000);  // poll every 10 s
         }
-        Microbot.log("Inventory should now be full of fish");
         // Drop the Big Fishing Net
         Rs2Inventory.drop(ItemID.BIG_NET);
     }
 
     private void cookBream() {
-        Microbot.log("Walking to cooking stove");
+        Microbot.log("Walking to cooking spot");
         Rs2Walker.walkFastCanvas(new WorldPoint(1512, 9693, 0));
-
-        while (Rs2Inventory.contains(ItemID.BREAM_FISH_RAW)) {
-
-            if (!Rs2Player.isAnimating()) {
-                // Not cooking right now – try to restart
-                if (Rs2GameObject.interact(ObjectID.PMOON_RANGE, "Cook")) {
-                    sleep(600, 900); // human-like pause to start animation
-                } else {
-                    Microbot.log("Range interact failed; will retry.");
-                }
+        for (int i = 0; i < 3 && Rs2Inventory.contains(ItemID.BREAM_FISH_RAW); i++) {
+            Microbot.log("Attempting to cook raw bream");
+            if (Rs2GameObject.interact(ObjectID.PMOON_RANGE, "Cook")) {
+                Microbot.log("Waiting for no more raw bream in inventory");
+                sleepUntil(() -> !Rs2Inventory.contains(ItemID.BREAM_FISH_RAW), 15_000);
             }
-
-            sleep(300, 500); // small poll interval
         }
-        Microbot.log("Finished cooking bream.");
     }
 
     private void rechargeRunEnergy(WorldPoint WorldPoint) {
@@ -193,20 +195,31 @@ public class moonsOfPerilScript extends Script {
         return moonlightPotionsQuantum - countMoonlightPotions();
     }
 
-    private void retrieveTravelItems() {
-        if (Rs2Bank.openBank()) {
-            Rs2Bank.withdrawOne(ItemID.FIRERUNE);
-            Rs2Bank.withdrawOne(ItemID.EARTHRUNE);
-            Rs2Bank.withdrawX(ItemID.EARTHRUNE, 2);
-            Rs2Bank.closeBank();
+    private boolean retrieveTravelItems() {
+        if (Rs2Bank.walkToBank()) {
+            if (Rs2Bank.openBank()) {
+                sleep(600,800);
+                Rs2Bank.withdrawOne(ItemID.FIRERUNE);
+                Rs2Inventory.waitForInventoryChanges(600);
+                Rs2Bank.withdrawOne(ItemID.EARTHRUNE);
+                Rs2Inventory.waitForInventoryChanges(600);
+                Rs2Bank.withdrawX(ItemID.LAWRUNE, 2);
+                Rs2Inventory.waitForInventoryChanges(600);
+                Rs2Bank.closeBank();
+            }
         }
+        else {
+            return false;
+        }
+        return true;
     }
 
-    private void retrieveDeathItems() {
+    private boolean retrieveDeathItems() {
         Microbot.log("Attempting to walk back to grave site");
         Rs2Walker.walkTo(1440, 9626, 0, 0);
-        Rs2Npc.interact(NpcID.GRAVESTONE_DEFAULT, "Loot");
         // interact with NPC grave site "loot"
+        Rs2Npc.interact(NpcID.GRAVESTONE_DEFAULT, "Loot");
+        return Rs2Inventory.waitForInventoryChanges(2000);
     }
 
     @Override
@@ -214,3 +227,5 @@ public class moonsOfPerilScript extends Script {
         super.shutdown();
     }
 }
+
+// [{"regionId":5782,"regionX":4,"regionY":32,"z":0,"color":"#FFFF0043","label":"Slash Boss"},{"regionId":5526,"regionX":47,"regionY":29,"z":0,"color":"#FFF2E836","label":"After jaguars"},{"regionId":5526,"regionX":45,"regionY":31,"z":0,"color":"#FFF2E836","label":"1"},{"regionId":5526,"regionX":45,"regionY":33,"z":0,"color":"#FFF2E836","label":"2"},{"regionId":5526,"regionX":51,"regionY":33,"z":0,"color":"#FFF2E836","label":"1"},{"regionId":5526,"regionX":51,"regionY":31,"z":0,"color":"#FFF2E836","label":"2"},{"regionId":5526,"regionX":49,"regionY":35,"z":0,"color":"#FFF2E836","label":"After Rain"},{"regionId":5782,"regionX":32,"regionY":59,"z":0,"color":"#FF00B8D4","label":"crush Boss"},{"regionId":5783,"regionX":31,"regionY":13,"z":0,"color":"#FFF2E836","label":"After tornado"},{"regionId":5783,"regionX":29,"regionY":15,"z":0,"color":"#FFF2E836","label":"1"},{"regionId":5783,"regionX":29,"regionY":17,"z":0,"color":"#FFF2E836","label":"2"},{"regionId":5783,"regionX":35,"regionY":17,"z":0,"color":"#FFF2E836","label":"1"},{"regionId":5783,"regionX":35,"regionY":15,"z":0,"color":"#FFF2E836","label":"2"},{"regionId":5783,"regionX":33,"regionY":19,"z":0,"color":"#FFF2E836","label":"After Frozen Weapons"},{"regionId":5782,"regionX":59,"regionY":32,"z":0,"color":"#FFFF9A00","label":"Stab Boss"},{"regionId":6038,"regionX":15,"regionY":29,"z":0,"color":"#FFF2E836","label":"After Orbit"},{"regionId":6038,"regionX":21,"regionY":37,"z":0,"color":"#FFF2E836"},{"regionId":6038,"regionX":11,"regionY":27,"z":0,"color":"#FFF2E836"},{"regionId":6038,"regionX":11,"regionY":37,"z":0,"color":"#FFF2E836"},{"regionId":6038,"regionX":21,"regionY":27,"z":0,"color":"#FFF2E836"},{"regionId":6038,"regionX":19,"regionY":27,"z":0,"color":"#FFF2E836","label":"walk"},{"regionId":6038,"regionX":17,"regionY":35,"z":0,"color":"#FFF2E836","label":"After Whack a Mole"},{"regionId":6038,"regionX":19,"regionY":33,"z":0,"color":"#FFF2E836","label":"1"},{"regionId":6038,"regionX":19,"regionY":31,"z":0,"color":"#FFF2E836","label":"2"},{"regionId":6038,"regionX":13,"regionY":31,"z":0,"color":"#FFF2E836","label":"1"},{"regionId":6038,"regionX":13,"regionY":33,"z":0,"color":"#FFF2E836","label":"2"},{"regionId":6038,"regionX":16,"regionY":32,"z":0,"color":"#FFF2E836","label":"Whack a Mole"}]
