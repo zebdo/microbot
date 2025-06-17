037920b30db93ff8395a3b6445a42f1088ae8e5f
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-06-17 01:24:53 -0400

    chore (agility): add missing import

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/agility/courses/AgilityCourseHandler.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/agility/courses/AgilityCourseHandler.java
@@ -1,5 +1,6 @@
 package net.runelite.client.plugins.microbot.agility.courses;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
