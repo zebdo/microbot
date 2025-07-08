6cb6aebf32b46e219336865f9decf1e4e922eab7
Simpamus Maximus <>
2025-07-07 22:05:11 -0400

    fix for dodgy necklace not being checked causing player to be stuck at bank, removed deprecated function usage, added world hopping config back for distracted wealthy citizens, updated plugin descriptions

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/maxxin/housethieving/HouseThievingConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/maxxin/housethieving/HouseThievingConfig.java
@@ -5,19 +5,24 @@ import net.runelite.client.plugins.microbot.util.misc.Rs2Food;
 
 @ConfigGroup(HouseThievingConfig.configGroup)
 @ConfigInformation(
-        "Plugin to thief houses in Varlamore. <br/>" +
-        "Only requirement is completion of Children of the Sun quest to access Varlamore. <br/>"
+        "Plugin to thief houses in Varlamore. Requirements are 50 Thieving and completion of Children of the Sun quest to access Varlamore. <br/>" +
+        "The plugin will pickpocket until max house keys gathered and then thieve houses until the min house keys remain. <br/>" +
+        "It will steal from wealthy citizens near Aurelia until a distracted citizen event occurs, prioritize that. <br/>" +
+        "Using Rogue's outfit is recommended, and food is recommended while dodgy necklaces are optional.<br/>" +
+        "World hop can be enabled to not require food at all, but maybe slower in gathering house keys.<br/>"
 )
 public interface HouseThievingConfig extends Config {
     String configGroup = "house-thieving";
 
     String maxHouseKeys = "maxHouseKeys";
     String minHouseKeys = "minHouseKeys";
-    String pickpocketFoodAmount = "pickpocketWaitTime";
+    String pickpocketFoodAmount = "pickpocketFoodAmount";
     String foodSelection = "foodSelection";
     String foodEatPercentage = "foodEatPercentage";
     String useDodgyNecklace = "useDodgyNecklace";
     String dodgyNecklaceAmount = "dodgyNecklaceAmount";
+    String worldHopForDistractedCitizens = "worldHopForDistractedCitizens";
+    String worldHopWaitTime = "worldHopWaitTime";
 
     @ConfigSection(
             name = "House Thieving Settings",
@@ -100,6 +105,28 @@ public interface HouseThievingConfig extends Config {
             section = generalSection
     )
     default int dodgyNecklaceAmount() {
+        return 5;
+    }
+
+    @ConfigItem(
+            keyName = worldHopForDistractedCitizens,
+            name = "World Hop For Distracted Citizens",
+            description = "World hop for Aurelia distracted citizen event",
+            position = 8,
+            section = generalSection
+    )
+    default boolean worldHopForDistractedCitizens() {
+        return false;
+    }
+
+    @ConfigItem(
+            keyName = worldHopWaitTime,
+            name = "World Hop Wait Time",
+            description = "How long to wait before world hopping for distracted ctizens",
+            position = 9,
+            section = generalSection
+    )
+    default int worldHopWaitTime() {
         return 5;
     }
 }
