6a2bc9c056c8919051382ae5c155dfdf1eed50b4
TheHaliax <The.Haliax@gmail.com>
2025-07-08 13:25:32 -0500

    Update FlaxSpinScript.java

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/crafting/scripts/FlaxSpinScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/crafting/scripts/FlaxSpinScript.java
@@ -16,9 +16,11 @@ import net.runelite.client.plugins.microbot.util.widget.Rs2Widget;
 
 import java.awt.event.KeyEvent;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+import static net.runelite.client.plugins.microbot.util.Global.sleepUntilTrue;
 
 enum State {
     SPINNING,
