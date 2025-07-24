f34f11cb3e56ab2a06186ee310d0abfcf49875d9
Gage307 <gagem44@gmail.com>
2025-07-24 12:38:40 -0400

    fix(Rs2Dialogue): widget child id for isInDialogue() updated to 566

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/dialogues/Rs2Dialogue.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/dialogues/Rs2Dialogue.java
@@ -25,7 +25,7 @@ public class Rs2Dialogue {
      * @return true if any dialogue-related widget is visible and the scroll bar is not visible, false otherwise.
      */
     public static boolean isInDialogue() {
-        return !Rs2Widget.isWidgetVisible(162, 558) && (hasContinue() || hasSelectAnOption());
+        return !Rs2Widget.isWidgetVisible(162, 566) && (hasContinue() || hasSelectAnOption());
     }
 
     /**
