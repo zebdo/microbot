c86ce5224587b96b929196f7c566d7cf655ff03c
SP <osrscardew@gmail.com>
2025-07-25 05:53:27 +1200

    Added public static boolean bankPinBeingHandled in Microbot.java. Allows other plugins to respect when this plugin needs to handle a bank pin.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/Microbot.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/Microbot.java
@@ -94,6 +94,7 @@ public class Microbot {
 	private static final SpecialAttackConfigs specialAttackConfigs = new SpecialAttackConfigs();
 	public static MenuEntry targetMenu;
 	public static boolean isGainingExp = false;
+	public static boolean bankPinBeingHandled = false;
 	public static AtomicBoolean pauseAllScripts = new AtomicBoolean(false);
 	public static String status = "IDLE";
 
