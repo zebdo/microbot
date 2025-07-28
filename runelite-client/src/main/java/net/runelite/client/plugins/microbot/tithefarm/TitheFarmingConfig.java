7537d6885755adabdf0f2303cd3178f4f1ac36b7
Igor <github@bolado.dev>
2025-07-28 19:44:41 +0200

    feat(TitheFarm): Added BreakHandler and Antiban support - Fixed some typos - Added BreakHandler support - Added Antiban features (optional, disabled by default) - Fixed the use of @ConfigItem fields instead of ConfigInformation for explanation about the plugin

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/tithefarm/TitheFarmingConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/tithefarm/TitheFarmingConfig.java
@@ -1,103 +1,113 @@
 package net.runelite.client.plugins.microbot.tithefarm;
 
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
-import net.runelite.client.config.ConfigSection;
+import net.runelite.client.config.*;
 import net.runelite.client.plugins.microbot.tithefarm.enums.TitheFarmLanes;
 
 @ConfigGroup(TitheFarmingConfig.GROUP)
+@ConfigInformation("<b>-- GUIDE --</b><br/><br/>" +
+"• Start at the entrance near the table to get seeds.<br />" +
+"• Have these items in your inventory:<br/>" +
+"&nbsp;&nbsp;8x Watering Can(8) or the Gricoller's Can.<br/>" +
+"&nbsp;&nbsp;Seed dibber.<br />" +
+"&nbsp;&nbsp;Spade.<br />" +
+"&nbsp;&nbsp;Stamina potions (optional)<br /><br />" +
+"• Make sure to have the tithe farm plugin from runelite enabled.<br />" +
+"<b>• At least level 34 farming required.</b><br />" +
+"• Happy botting!<br />")
 public interface TitheFarmingConfig extends Config {
 
     String GROUP = "Farming";
 
-    @ConfigItem(
-            keyName = "guide",
-            name = "How to use",
-            description = "How to use this plugin",
-            position = 0
-    )
-    default String GUIDE() {
-        return "Start at the entrance near the table to get seeds\n" +
-                "Make sure to have 8 x watering can(8) or Gricoller's can, seed dibber, spade in inventory\n" +
-                "Stamina potions are supported!\n" +
-                "Make sure to have the tithe farm plugin from runelite enabled\n" +
-                "Happy botting!";
-    }
-
     @ConfigSection(
             name = "Script Settings",
             description = "General",
-            position = 1,
+            position = 0,
             closedByDefault = false
     )
     String scriptSettings = "Script Settings";
+
     @ConfigItem(
             keyName = "storing",
-            name = "Store fruit treshhold",
+            name = "Store fruit threshold",
             description = "Amount of fruits to have in your inventory before storing them in the sack",
             position = 0,
             section = scriptSettings
     )
-    default int storeFruitTreshhold() {
+    default int storeFruitThreshold() {
         return 100;
     }
+
     @ConfigItem(
             keyName = "Lanes",
             name = "Tithe farm lanes",
             description = "Choose a lane starting from the entrance",
-            position = 0,
+            position = 1,
             section = scriptSettings
     )
     default TitheFarmLanes Lanes() {
         return TitheFarmLanes.LANE_2_3;
     }
+
     @ConfigItem(
-            keyName = "Gricoller's can refill treshhold",
-            name = "Gricoller's can refill treshhold",
+            keyName = "Gricoller's can refill threshold",
+            name = "Gricoller's can refill threshold",
             description = "Percentage before refilling the gricoller's can",
-            position = 1,
+            position = 2,
             section = scriptSettings
     )
-    default int gricollerCanRefillTreshhold() {
+    default int gricollerCanRefillThreshold() {
         return 30;
     }
+
     @ConfigItem(
             keyName = "Sleep after planting seed",
             name = "Sleep after planting seed",
             description = "Sleep after planting seed - changing this value might result in unexpected behavior",
-            position = 2,
+            position = 3,
             section = scriptSettings
     )
     default int sleepAfterPlantingSeed() {
         return 2000;
     }
+
     @ConfigItem(
             keyName = "Sleep after watering seed",
             name = "Sleep after watering seed",
             description = "Sleep after watering seed - changing this value might result in unexpected behavior",
-            position = 3,
+            position = 4,
             section = scriptSettings
     )
     default int sleepAfterWateringSeed() {
         return 2000;
     }
+
     @ConfigItem(
             keyName = "Sleep after harvesting seed",
             name = "Sleep after harvesting seed",
             description = "Sleep after harvesting seed - changing this value might result in unexpected behavior",
-            position = 4,
+            position = 5,
             section = scriptSettings
     )
     default int sleepAfterHarvestingSeed() {
         return 2000;
     }
 
+    @ConfigItem(
+            keyName = "enableAntiban",
+            name = "Enable Antiban Features",
+            description = "Will enable features like natural mouse movements and such.",
+            position = 6,
+            section = scriptSettings
+    )
+    default boolean enableAntiban() {
+        return false;
+    }
+
 
     @ConfigSection(
             name = "Debug Settings",
             description = "General",
-            position = 2,
+            position = 1,
             closedByDefault = false
     )
     String debugSettings = "Debug Settings";
@@ -106,7 +116,7 @@ public interface TitheFarmingConfig extends Config {
             keyName = "Enable Debug",
             name = "Enable Debug",
             description = "Enable debugger",
-            position = 1,
+            position = 0,
             section = debugSettings
     )
     default boolean enableDebugging() {
