42201bf1bb0a7a12c9fd55e1a963895bed9214bb
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-07-29 16:03:04 -0400

    chore(spirit-tree): remove breakpoint debug statement

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/cache/strategy/farming/SpiritTreeUpdateStrategy.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/cache/strategy/farming/SpiritTreeUpdateStrategy.java
@@ -121,7 +121,6 @@ public class SpiritTreeUpdateStrategy implements CacheUpdateStrategy<SpiritTree,
         if (SPIRIT_TREE_VARBIT_IDS.contains(varbitId)) {
             log.debug("Spirit tree farming varbit {} changed to value {}, updating spirit tree farming states", varbitId, event.getValue());            
             // Update farming states for all farmable spirit tree patches
-			System.out.println("break");
             updateFarmingStatesFromVarbits(cache);
         }
     }
