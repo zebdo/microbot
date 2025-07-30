e67e8f5409ce7d1c8b1edf4dab41e3550af68a0b
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-07-30 09:20:17 -0400

    feat(agility): add teleport to seers village feature, will consider if you have the required runes & diary completed

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/agility/courses/SeersCourse.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/agility/courses/SeersCourse.java
@@ -3,7 +3,13 @@ package net.runelite.client.plugins.microbot.agility.courses;
 import java.util.List;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.gameval.ObjectID;
+import net.runelite.api.gameval.VarbitID;
+import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.agility.models.AgilityObstacleModel;
+import net.runelite.client.plugins.microbot.util.Global;
+import net.runelite.client.plugins.microbot.util.magic.Rs2Magic;
+import net.runelite.client.plugins.microbot.util.magic.Rs2Spells;
+import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 
 public class SeersCourse implements AgilityCourseHandler
 {
@@ -30,5 +36,38 @@ public class SeersCourse implements AgilityCourseHandler
 	public Integer getRequiredLevel()
 	{
 		return 60;
+	}
+
+	@Override
+	public boolean handleWalkToStart(WorldPoint playerWorldLocation)
+	{
+		if (Microbot.getClient().getTopLevelWorldView().getPlane() != 0)
+		{
+			return false;
+		}
+		if (getCurrentObstacleIndex() > 0)
+		{
+			return false;
+		}
+
+		if (Microbot.getVarbitValue(VarbitID.KANDARIN_DIARY_HARD_COMPLETE) == 1
+			&& Rs2Magic.hasRequiredRunes(Rs2Spells.CAMELOT_TELEPORT)
+			&& playerWorldLocation.distanceTo(getStartPoint()) > 12)
+
+		{
+			Rs2Magic.cast(Rs2Spells.CAMELOT_TELEPORT, "Seers'", 2);
+			return Global.sleepUntil(() -> {
+				WorldPoint currentLocation = Microbot.getClient().getLocalPlayer().getWorldLocation();
+				return currentLocation.distanceTo(getStartPoint()) <= 12;
+			}, 5000);
+		}
+
+		if (playerWorldLocation.distanceTo(getStartPoint()) > 12)
+		{
+			Microbot.log("Going back to course's starting point");
+			Rs2Walker.walkTo(getStartPoint(), 2);
+			return true;
+		}
+		return false;
 	}
 }
