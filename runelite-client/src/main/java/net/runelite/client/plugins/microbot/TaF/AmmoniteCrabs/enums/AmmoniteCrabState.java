7bfa802df3ec23a34cb2df785fd396fd763c99bf
Steffen Carlsen <bstaffe@gmail.com>
2025-08-03 00:48:01 +0200

    feat(AmmoniteCrabs): Add ammonite crab script

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/AmmoniteCrabs/enums/AmmoniteCrabState.java
@@ -0,0 +1,11 @@
+package net.runelite.client.plugins.microbot.TaF.AmmoniteCrabs.enums;
+
+public enum AmmoniteCrabState {
+    FIGHT,
+    AFK,
+    RESET_AGGRO,
+    WALK_BACK,
+    HOP_WORLD,
+    BANK,
+    SCANNING_WORLD
+}
