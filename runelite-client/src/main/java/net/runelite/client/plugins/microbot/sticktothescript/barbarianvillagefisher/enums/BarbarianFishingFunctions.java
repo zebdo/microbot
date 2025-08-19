4ee3816b38f4682de43bcc2923990b535670480d
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-08-19 01:59:09 -0400

    chore: remove STTS

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/sticktothescript/barbarianvillagefisher/enums/BarbarianFishingFunctions.java
+++
@@ -1,8 +0,0 @@
-package net.runelite.client.plugins.microbot.sticktothescript.barbarianvillagefisher.enums;
-
-public enum BarbarianFishingFunctions {
-    DROP_RAW,
-    COOK_AND_DROP,
-    COOK_AND_BANK,
-    BANK_RAW;
-}
