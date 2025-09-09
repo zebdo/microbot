1372ddaa88b3e1fbf9545bd52acf82bf8556c836
krulvis <joepkleint@gmail.com>
2025-09-09 23:05:48 +0200

    chore: Add documentation

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/poh/PohTransport.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/poh/PohTransport.java
@@ -7,22 +7,39 @@ import net.runelite.client.plugins.microbot.shortestpath.TransportType;
 import net.runelite.client.plugins.microbot.util.poh.data.HouseStyle;
 import net.runelite.client.plugins.microbot.util.poh.data.PohTeleport;
 
+/**
+ * Represents a transport mechanism using the Player-Owned House (POH) teleportation system.
+ * This class extends the base Transport class and provides specific implementations
+ * for POH teleportation.
+ */
 public class PohTransport extends Transport {
 
     @Getter
     private final PohTeleport teleport;
+    private final WorldPoint exitPortalPoint;
 
     public PohTransport(PohTeleport teleport) {
         super(teleport.getDestination(), teleport.displayInfo(), TransportType.POH);
         this.teleport = teleport;
+        HouseStyle style = HouseStyle.getStyle();
+        this.exitPortalPoint = style != null ? style.getPohLocation() : new WorldPoint(-1, -1, -1);
     }
 
+    /**
+     * The origin of a PoH transport is the WorldPoint inside Player-Owned House (POH) on entering
+     *
+     * @return WorldPoint N.W. of Exit Portal
+     */
     @Override
     public WorldPoint getOrigin() {
-        HouseStyle style = HouseStyle.getStyle();
-        return style != null ? style.getPohLocation() : new WorldPoint(-1, -1, -1);
+        return exitPortalPoint;
     }
 
+    /**
+     * Executes the Transport's PoH teleportation action.
+     *
+     * @return true on successful teleportation
+     */
     public boolean execute() {
         return teleport.execute();
     }
