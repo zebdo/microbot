9114eee114c734ede3478b57d6fd69d62dcd3d89
chsami <sami.chkhachkhi@gmail.com>
2025-09-13 22:55:59 +0200

    feat: remove bradleycombat

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/bradleycombat/enums/PrayerStyle.java
+++
@@ -1,7 +0,0 @@
-package net.runelite.client.plugins.microbot.bradleycombat.enums;
-
-public enum PrayerStyle {
-    MELEE,
-    RANGE,
-    MAGE
-}
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/bradleycombat/enums/SpecType.java
+++
@@ -1,6 +0,0 @@
-package net.runelite.client.plugins.microbot.bradleycombat.enums;
-
-public enum SpecType {
-    SINGLE,
-    DOUBLE
-}
