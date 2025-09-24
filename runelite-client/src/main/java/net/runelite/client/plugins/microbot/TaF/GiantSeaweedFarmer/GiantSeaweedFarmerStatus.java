054923aeba5b4b3e5eef0201f7d086ed3c879eeb
chsami <sami.chkhachkhi@gmail.com>
2025-09-24 19:27:26 +0200

    feat: migrated taf plugins to the hub

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/GiantSeaweedFarmer/GiantSeaweedFarmerStatus.java
+++
@@ -1,8 +0,0 @@
-package net.runelite.client.plugins.microbot.TaF.GiantSeaweedFarmer;
-
-public enum GiantSeaweedFarmerStatus {
-    BANKING,
-    WALKING_TO_PATCH,
-    FARMING,
-    RETURN_TO_BANK
-}
