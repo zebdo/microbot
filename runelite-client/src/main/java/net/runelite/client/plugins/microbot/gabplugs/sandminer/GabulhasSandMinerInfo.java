cf002cfed36d4034f575fa6c0c6ec216fe3f57bf
chsami <sami.chkhachkhi@gmail.com>
2025-09-24 21:00:05 +0200

    feat: migrated more plugins

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/gabplugs/sandminer/GabulhasSandMinerInfo.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.microbot.gabplugs.sandminer;
-
-public class GabulhasSandMinerInfo {
-    public static states botStatus;
-    public enum states {
-        Mining,
-        Depositing
-    }
-}
