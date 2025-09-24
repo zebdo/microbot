3576edb166d4b34daf4faf739bfcdfe05eae6e88
chsami <sami.chkhachkhi@gmail.com>
2025-09-24 20:38:40 +0200

    feat: migrated more plugins

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/zerozero/bluedragons/BlueDragonState.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.microbot.zerozero.bluedragons;
-
-public enum BlueDragonState {
-    BANKING,
-    TRAVEL_TO_DRAGONS,
-    FIGHTING,
-    LOOTING,
-    STARTING
-}
