5431f4ee91a5990388309c01845f9f18e78d783b
chsami <sami.chkhachkhi@gmail.com>
2025-09-25 13:57:15 +0200

    chore: remove unused code and use keyboard to switch from tab

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/globval/VarcStrIndices.java
+++
@@ -1,5 +0,0 @@
-package net.runelite.client.plugins.microbot.globval;
-
-public class VarcStrIndices {
-    public static final int REPORT_PLAYER_NAME = 370;
-}
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/globval/enums/ViewportLayout.java
+++
@@ -1,7 +0,0 @@
-package net.runelite.client.plugins.microbot.globval.enums;
-
-public enum ViewportLayout {
-    FIXED_CLASSIC,
-    RESIZABLE_CLASSIC,
-    RESIZABLE_MODERN,
-}
