b5ea8dcc6da35991c77b83f375ac0ed42e58a246
chsami <sami.chkhachkhi@gmail.com>
2025-11-14 05:55:52 +0100

    feat(tileobjects): implement tile object querying and caching

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/api/tileobject/Rs2TileObjectQueryable.java
@@ -0,0 +1,83 @@
+package net.runelite.client.plugins.microbot.api.tileobject;
+
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.plugins.microbot.api.IEntityQueryable;
+import net.runelite.client.plugins.microbot.api.tileobject.models.Rs2TileObjectModel;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public final class Rs2TileObjectQueryable
+        implements IEntityQueryable<Rs2TileObjectQueryable, Rs2TileObjectModel> {
+
+    private Stream<Rs2TileObjectModel> source;
+
+    public Rs2TileObjectQueryable() {
+        this.source = Rs2TileObjectCache.getObjectsStream();
+    }
+
+    @Override
+    public Rs2TileObjectQueryable where(java.util.function.Predicate<Rs2TileObjectModel> predicate) {
+       source = source.filter(predicate);
+       return this;
+    }
+
+    @Override
+    public Rs2TileObjectModel first() {
+        return source.findFirst().orElse(null);
+    }
+
+    @Override
+    public Rs2TileObjectModel nearest() {
+        WorldPoint playerLoc = Rs2Player.getWorldLocation();
+        if (playerLoc == null) {
+            return null;
+        }
+
+        return source
+                .min(java.util.Comparator.comparingInt(
+                        o -> o.getWorldLocation().distanceTo(playerLoc)))
+                .orElse(null);
+    }
+
+    @Override
+    public Rs2TileObjectModel nearest(int maxDistance) {
+        WorldPoint playerLoc = Rs2Player.getWorldLocation();
+        if (playerLoc == null) {
+            return null;
+        }
+
+        return source
+                .filter(x -> x.getWorldLocation().distanceTo(playerLoc) <= maxDistance)
+                .min(java.util.Comparator.comparingInt(
+                        o -> o.getWorldLocation().distanceTo(playerLoc)))
+                .orElse(null);
+    }
+
+    @Override
+    public Rs2TileObjectModel withName(String name) {
+        return source.filter(x -> x.getName().toLowerCase() == name.toLowerCase()).findFirst().orElse(null);
+    }
+
+    @Override
+    public Rs2TileObjectModel withNames(String... names) {
+        return null;
+    }
+
+    @Override
+    public Rs2TileObjectModel withId(int id) {
+        return null;
+    }
+
+    @Override
+    public Rs2TileObjectModel withIds(int... ids) {
+        return null;
+    }
+
+    @Override
+    public java.util.List<Rs2TileObjectModel> toList() {
+        return source.collect(Collectors.toList());
+    }
+}
