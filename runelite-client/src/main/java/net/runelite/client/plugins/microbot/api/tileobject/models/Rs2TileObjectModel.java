a343bed36b69d20df45324693448293b614d24a7
chsami <sami.chkhachkhi@gmail.com>
2025-11-30 15:44:05 +0100

    feat(tileObject): add operation override methods for enhanced functionality

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/api/tileobject/models/Rs2TileObjectModel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/api/tileobject/models/Rs2TileObjectModel.java
@@ -140,6 +140,16 @@ public class Rs2TileObjectModel implements TileObject, IEntity {
         return tileObject.getClickbox();
     }
 
+    @Override
+    public @Nullable String getOpOverride(int index) {
+        return tileObject.getOpOverride(index);
+    }
+
+    @Override
+    public boolean isOpShown(int index) {
+        return tileObject.isOpShown(index);
+    }
+
     public ObjectComposition getObjectComposition() {
         return Microbot.getClientThread().invoke(() -> {
             ObjectComposition composition = Microbot.getClient().getObjectDefinition(tileObject.getId());
